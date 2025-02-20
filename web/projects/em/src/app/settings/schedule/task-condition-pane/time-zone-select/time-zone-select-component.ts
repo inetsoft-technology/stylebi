d90d805b4f562b049c18bc06a5c2cb08c0179651
catherineli <catherineli@inetsoft.com>
2025-02-20 11:17:07 +0800

    fix Bug #70126: should set default to select local timem zone not server time zone, in normal cases, we want using local time zone to set time.

--- web/projects/em/src/app/settings/schedule/task-condition-pane/time-zone-select/time-zone-select-component.ts
+++ web/projects/em/src/app/settings/schedule/task-condition-pane/time-zone-select/time-zone-select-component.ts
@@ -75,8 +75,7 @@ export class TimeZoneSelectComponent implements OnInit, ControlValueAccessor {
 
    setTimeZoneLabel(changed: boolean) {
       let timeZoneLabel = this.dateTimeService
-         .getTimeZoneLabel(this.timeZoneOptions, this.timeZoneId,
-         this.timeZoneOptions != null ? this.timeZoneOptions[0].timeZoneId : null);
+         .getTimeZoneLabel(this.timeZoneOptions, this.timeZoneId, this.timeZoneOptions[0].timeZoneId);
       this.labelChanged.emit(timeZoneLabel);
 
       if(changed) {
