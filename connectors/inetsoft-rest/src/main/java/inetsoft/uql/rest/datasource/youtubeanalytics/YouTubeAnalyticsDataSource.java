0f9221e698dc16b97ca092a0348d30b3a44ec842
frankypaninetsoft <franky.pan@inetsoft.com>
2025-02-21 10:31:17 -0500

    Bug #70142 Remove unnecessary scopes and endpoints from Youtube Analytics connector

--- connectors/inetsoft-rest/src/main/java/inetsoft/uql/rest/datasource/youtubeanalytics/YouTubeAnalyticsDataSource.java
+++ connectors/inetsoft-rest/src/main/java/inetsoft/uql/rest/datasource/youtubeanalytics/YouTubeAnalyticsDataSource.java
@@ -98,9 +98,7 @@ public class YouTubeAnalyticsDataSource
    @Override
    public String getScope() {
       return "https://www.googleapis.com/auth/yt-analytics.readonly " +
-         "https://www.googleapis.com/auth/yt-analytics-monetary.readonly " +
-         "https://www.googleapis.com/auth/youtube " +
-         "https://www.googleapis.com/auth/youtubepartner";
+         "https://www.googleapis.com/auth/yt-analytics-monetary.readonly";
    }
 
    @Override
