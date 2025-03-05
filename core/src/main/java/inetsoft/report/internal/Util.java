3e674e2e55d750f8a76c8b9d11d184a88a6216c6
catherineli <catherineli@inetsoft.com>
2025-03-05 09:56:29 +0800

    fix Bug #70307: when auto save file path will have user identity with organzation id, we should only show object name without organization id becaused we have resource organzation to filter data.

--- core/src/main/java/inetsoft/report/internal/Util.java
+++ core/src/main/java/inetsoft/report/internal/Util.java
@@ -2752,7 +2752,13 @@ public class Util implements inetsoft.report.StyleConstants {
             return path;
          }
 
-         return "Recycle Bin/Auto Saved Files/" + strings[2] + "/Dashboard/" + strings[3];
+         String userName = strings[2];
+
+         if(userName.contains(IdentityID.KEY_DELIMITER)) {
+             userName = IdentityID.getIdentityIDFromKey(userName).name;
+         }
+
+         return "Recycle Bin/Auto Saved Files/" + userName + "/Dashboard/" + strings[3];
       case RepositoryEntry.AUTO_SAVE_WS:
          if(path == null) {
             return "";
