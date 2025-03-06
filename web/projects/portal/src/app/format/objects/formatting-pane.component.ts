05698b36e38ac00cba638fd049078bb7d45e1be9
beauzhu <beauzhu@inetsoft.com>
2025-03-06 13:05:43 +0800

    Fix Bug #70333: When replacing or modifying the userformat, it is necessary to check whether the previously set format exists. If it does not exist, the model's format should be cleared.

--- web/projects/portal/src/app/format/objects/formatting-pane.component.ts
+++ web/projects/portal/src/app/format/objects/formatting-pane.component.ts
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { FormatInfoModel } from "../../common/data/format-info-model";
 import { LabelValueTuple } from "../../../../../shared/util/label-value-tuple";
 import { Format } from "../../common/util/format";
@@ -29,7 +29,7 @@ import {
    templateUrl: "formatting-pane.component.html",
    styleUrls: ["formatting-pane.component.scss"]
 })
-export class FormattingPane {
+export class FormattingPane implements OnInit {
    @Input() formatModel: FormatInfoModel;
    @Input() dynamic: boolean = false;
    @Input() vsId: string = null;
@@ -72,6 +72,17 @@ export class FormattingPane {
          "\u00A4#,##0.00;(\u00A4#,##0.00)"];
 
       this.durationFmts = ["", "dd HH:mm", "dd HH:mm:ss", "HH:mm", "HH:mm:ss", "mm", "mm:ss", "ss"];
+   }
+
+   ngOnInit() {
+      this.getDecimalFormats();
+
+      if(this.formatModel.format == "DecimalFormat") {
+         if(this.decimalFmts.indexOf(this.formatModel.formatSpec) == -1) {
+            this.formatModel.format = null;
+            this.formatModel.formatSpec = null;
+         }
+      }
    }
 
    getDecimalFormats(): string[] {
