dc81dfb007a8547344d597ea0ed09df4e009cbcc
Nicholas <nicholas.govus@inetsoft.com>
2025-03-14 15:28:35 -0600

    update code style

--- core/src/main/java/inetsoft/web/admin/schedule/ScheduleCycleService.java
+++ core/src/main/java/inetsoft/web/admin/schedule/ScheduleCycleService.java
@@ -354,6 +354,7 @@ public class ScheduleCycleService {
       if(orgID == null || !SUtil.isMultiTenant() ) {
          orgID = Organization.getDefaultOrganizationID();
       }
+
       return new IdentityID(name, orgID).convertToKey();
    }
 
