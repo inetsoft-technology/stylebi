ba467c5fac2b4be3a8b3e70c85e4647426f34028
Nicholas <nicholas.govus@inetsoft.com>
2025-03-20 10:00:50 -0600

    Feature #70119, VSCalculatorController service Proxy

--- core/src/main/java/inetsoft/web/binding/controller/VSCalculatorController.java
+++ core/src/main/java/inetsoft/web/binding/controller/VSCalculatorController.java
@@ -49,175 +49,22 @@ import java.util.stream.Stream;
 public class VSCalculatorController {
 
    public VSCalculatorController(VSCalculatorServiceProxy vsCalculatorServiceProxy) {
-      this.vsCalculatorService = vsCalculatorServiceProxy;
+      this.vsCalculatorServiceProxy = vsCalculatorServiceProxy;
    }
 
    @RequestMapping(value = "/api/composer/dims", method = RequestMethod.GET)
    @ResponseBody
    public Map<String, List<DimensionInfo>> getDimensionInfos(@RequestParam("vsId") String vsId,
                                                @RequestParam("assemblyName") String assemblyName,
-                                               Principal principal)
-      throws Exception
+                                               Principal principal) throws Exception
    {
-      Assembly assembly = getAssembly(Tool.byteDecode(vsId), assemblyName, principal);
-      Map<String, List<DimensionInfo>> map = new HashMap<>();
-      map.put(CalculatorHandler.PERCENT_LEVEL_TAG, new ArrayList<>());
-      map.put(CalculatorHandler.PERCENT_DIMS_TAG, new ArrayList<>());
-      map.put(CalculatorHandler.VALUE_OF_TAG, new ArrayList<>());
-      map.put(CalculatorHandler.BREAK_BY_TAG, new ArrayList<>());
-      map.put(CalculatorHandler.MOVING_TAG, new ArrayList<>());
-      appendFixedOptions(map, assembly);
-
-      List<XDimensionRef> list = null;
-
-      if(assembly instanceof ChartVSAssembly) {
-         list = getAllDimensions((ChartVSAssembly) assembly);
-      }
-      else if(assembly instanceof CrosstabVSAssembly) {
-         list = getAllDimensions((CrosstabVSAssembly) assembly);
-      }
-
-      List<DimensionInfo> dinfos = new ArrayList<>();
-      List<String> variables = new ArrayList<>();
-
-      for(int i = 0; i < list.size(); i++) {
-         XDimensionRef dim = list.get(i);
-
-         if(!(dim instanceof VSDimensionRef)) {
-            continue;
-         }
-
-         VSDimensionRef vdim = (VSDimensionRef) dim;
-
-         if(vdim.isVariable()) {
-            String group = vdim.getGroupColumnValue();
-
-            if(variables.contains(group)) {
-               continue;
-            }
-
-            variables.add(group);
-         }
-
-         String desc = null;
-
-         if(dim.getDataRef() instanceof ColumnRef) {
-            desc = ((ColumnRef) dim.getDataRef()).getDescription();
-         }
-
-         dinfos.add(new DimensionInfo(dim, chartHandler, desc));
-      }
-
-      map.get(CalculatorHandler.PERCENT_DIMS_TAG).addAll(dinfos);
-      map.get(CalculatorHandler.VALUE_OF_TAG).addAll(dinfos);
-      map.get(CalculatorHandler.BREAK_BY_TAG).addAll(dinfos);
-
-      return map;
+      return vsCalculatorServiceProxy.getDimensionInfos(vsId, assemblyName, chartHandler, catalog, principal);
    }
 
-   private void appendFixedOptions(Map<String, List<DimensionInfo>> map, Assembly assembly) {
-      List<DimensionInfo> percLevels = map.get(CalculatorHandler.PERCENT_LEVEL_TAG);
-      List<DimensionInfo> valueOfDatas = map.get(CalculatorHandler.VALUE_OF_TAG);
-      List<DimensionInfo> breakByDims = map.get(CalculatorHandler.BREAK_BY_TAG);
-      List<DimensionInfo> movingDims = map.get(CalculatorHandler.MOVING_TAG);
 
-      if(assembly instanceof ChartVSAssembly) {
-         percLevels.add(new DimensionInfo(
-            catalog.getString("Grand Total"), PercentCalc.GRAND_TOTAL + ""));
-         percLevels.add(new DimensionInfo(
-            catalog.getString("Sub Total"), PercentCalc.SUB_TOTAL + ""));
-         valueOfDatas.add(new DimensionInfo(catalog.getString("Series"), ""));
-         breakByDims.add(
-            new DimensionInfo(catalog.getString("Inner Dimension (Default)"), ""));
-      }
-      else if(assembly instanceof CrosstabVSAssembly) {
-         percLevels.add(new DimensionInfo(
-            catalog.getString("Grand Total"), PercentCalc.GRAND_TOTAL + ""));
-         percLevels.add(new DimensionInfo(
-            catalog.getString("Sub Total"), PercentCalc.SUB_TOTAL + ""));
-
-         VSCrosstabInfo info = ((CrosstabVSAssembly) assembly).getVSCrosstabInfo();
-         DataRef[] rowHeaders = info.getRowHeaders();
-         DataRef[] colHeaders = info.getColHeaders();
-         boolean hasRow = rowHeaders != null && rowHeaders.length > 0;
-         boolean hasCol = colHeaders != null && colHeaders.length > 0;
-
-         if(hasRow) {
-            valueOfDatas.add(new DimensionInfo(
-               catalog.getString(ValueOfCalc.ROW_SERIES), AbstractCalc.ROW_INNER));
-            movingDims.add(new DimensionInfo(
-               catalog.getString("Row Inner Dimension"), AbstractCalc.ROW_INNER));
-            breakByDims.add(new DimensionInfo(
-               catalog.getString("Row Inner Dimension (Default)"), AbstractCalc.ROW_INNER));
-         }
-
-         if(hasCol) {
-            valueOfDatas.add(new DimensionInfo(
-               catalog.getString(ValueOfCalc.COLUMN_SERIES), AbstractCalc.COLUMN_INNER));
-            movingDims.add(new DimensionInfo(
-               catalog.getString("Column Inner Dimension"), AbstractCalc.COLUMN_INNER));
-            String label = "Column Inner Dimension";
-
-            if(!hasRow) {
-               label = label + " (Default)";
-            }
-
-            breakByDims.add(new DimensionInfo(catalog.getString(label), AbstractCalc.COLUMN_INNER));
-         }
-      }
-   }
-
-   private Assembly getAssembly(String vsId, String assemblyName, Principal principal)
-      throws Exception
+   private Assembly getAssembly(String vsId, String assemblyName, Principal principal) throws Exception
    {
-      return vsCalculatorService.getAssembly(vsId, assemblyName, principal);
-   }
-
-   private List<XDimensionRef> getAllDimensions(CrosstabVSAssembly crosstab) {
-      VSCrosstabInfo cinfo = crosstab.getVSCrosstabInfo();
-      DataRef[] rows = cinfo.getRuntimeRowHeaders();
-      DataRef[] cols = cinfo.getRuntimeColHeaders();
-      DataRef[] periodRows = cinfo.getPeriodRuntimeRowHeaders();
-
-      final List<XDimensionRef> dims = Stream.of(Stream.of(rows), Stream.of(cols), Stream.of(periodRows))
-                                                .flatMap(Function.identity())
-                                                .filter(ref -> ref instanceof XDimensionRef)
-                                                .map(XDimensionRef.class::cast)
-                                                .collect(Collectors.toList());
-
-      return getUniqueDimensions(dims);
-   }
-
-   private List<XDimensionRef> getAllDimensions(ChartVSAssembly chart) {
-      VSChartInfo cinfo = chart.getVSChartInfo();
-      List<XDimensionRef> dims = chartHandler.getAllDimensions(cinfo, true);
-      dims = getUniqueDimensions(dims);
-      fixDescription(chart, dims);
-      return dims;
-   }
-
-   private List<XDimensionRef> getUniqueDimensions(List<XDimensionRef> refs) {
-      final Set<String> names = new HashSet<>();
-      final List<XDimensionRef> uniqueRefs = new ArrayList<>();
-
-      for(XDimensionRef ref : refs) {
-         if(!names.contains(ref.getFullName())) {
-            uniqueRefs.add(ref);
-            names.add(ref.getFullName());
-         }
-      }
-
-      return uniqueRefs;
-   }
-
-   private void fixDescription(ChartVSAssembly chart, List<XDimensionRef> refs) {
-      ColumnSelection cols = new ColumnSelection();
-
-      for(int i = 0; i < refs.size(); i++) {
-         cols.addAttribute(new ColumnRef(refs.get(i)));
-      }
-
-      XUtil.addDescriptionsFromSource(chart, cols);
+      return vsCalculatorServiceProxy.getAssembly(vsId, assemblyName, principal);
    }
 
    @RequestMapping(value = "/api/composer/supportReset", method = RequestMethod.GET)
@@ -226,40 +73,10 @@ public class VSCalculatorController {
                                @RequestParam("aggreName") String aggreName, Principal principal)
       throws Exception
    {
-      Assembly assembly = getAssembly(Tool.byteDecode(vsId), assemblyName, principal);
-      Map map = new HashMap();
-
-      if(assembly instanceof ChartVSAssembly) {
-         VSAssemblyInfo info = ((ChartVSAssembly) assembly).getVSAssemblyInfo();
-         VSChartInfo cinfo = ((ChartVSAssemblyInfo) info).getVSChartInfo();
-         map.put(CalculatorHandler.INNER_DIMENSION, chartHandler.supportReset(cinfo, aggreName));
-      }
-      else if(assembly instanceof CrosstabVSAssembly) {
-         VSAssemblyInfo info = ((CrosstabVSAssembly) assembly).getVSAssemblyInfo();
-         VSCrosstabInfo cinfo = ((CrosstabVSAssemblyInfo) info).getVSCrosstabInfo();
-         map.put(AbstractCalc.ROW_INNER, supportReset(cinfo.getRowHeaders()));
-         map.put(AbstractCalc.COLUMN_INNER, supportReset(cinfo.getColHeaders()));
-      }
-
-      return map;
+      return vsCalculatorServiceProxy.supportReset(vsId, assemblyName, aggreName, chartHandler, calculatorHandler, principal);
    }
 
-   /**
-    * Return if support reset for running total.
-    */
-   private boolean supportReset(DataRef[] refs) {
-      if(refs == null || refs.length == 0) {
-         return false;
-      }
 
-      DataRef ref = refs[refs.length - 1];
-
-      if(!(ref instanceof XDimensionRef)) {
-         return false;
-      }
-
-      return calculatorHandler.supportReset((XDimensionRef) ref);
-   }
 
    @RequestMapping(value = "/api/composer/resetOptions", method = RequestMethod.GET)
    @ResponseBody
@@ -268,40 +85,13 @@ public class VSCalculatorController {
                                          @RequestParam("aggreName") String aggreName, Principal principal)
       throws Exception
    {
-      Assembly assembly = getAssembly(Tool.byteDecode(vsId), assemblyName, principal);
-      Map map = new HashMap();
-
-      if(assembly instanceof ChartVSAssembly) {
-         VSAssemblyInfo info = ((ChartVSAssembly) assembly).getVSAssemblyInfo();
-         VSChartInfo cinfo = ((ChartVSAssemblyInfo) info).getVSChartInfo();
-
-         map.put(CalculatorHandler.INNER_DIMENSION, chartHandler.getResetOptions(cinfo, aggreName));
-      }
-      else if(assembly instanceof CrosstabVSAssembly) {
-         VSAssemblyInfo info = ((CrosstabVSAssembly) assembly).getVSAssemblyInfo();
-         VSCrosstabInfo cinfo = ((CrosstabVSAssemblyInfo) info).getVSCrosstabInfo();
-         DataRef[] rowHeaders = cinfo.getRowHeaders();
-         DataRef[] colHeaders = cinfo.getColHeaders();
-         XDimensionRef dref = null;
-
-         if(supportReset(rowHeaders)) {
-            dref = (XDimensionRef) rowHeaders[rowHeaders.length - 1];
-            map.put(AbstractCalc.ROW_INNER, calculatorHandler.getResetOptions(dref));
-         }
-
-         if(supportReset(colHeaders)) {
-            dref = (XDimensionRef) colHeaders[colHeaders.length - 1];
-            map.put(AbstractCalc.COLUMN_INNER, calculatorHandler.getResetOptions(dref));
-         }
-      }
-
-      return map;
+      return vsCalculatorServiceProxy.getResetOptions(vsId, assemblyName, aggreName, principal);
    }
 
    @Autowired
    private VSChartHandler chartHandler;
    @Autowired
    private CalculatorHandler calculatorHandler;
-   private VSCalculatorServiceProxy vsCalculatorService;
+   private VSCalculatorServiceProxy vsCalculatorServiceProxy;
    private Catalog catalog = Catalog.getCatalog();
 }
