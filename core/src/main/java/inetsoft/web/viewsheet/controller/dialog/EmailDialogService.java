4519f7334362dd0841ea0cbd22d0662fbd7ff7c4
ngovus-inetsoft <nicholas.govus@inetsoft.com>
2025-03-24 07:48:17 -0600

    Feature #70119, EmailDialogController service proxy (#390)
    * Feature #70119, EmailDialogController service proxy
    
    * Feature #70119, EmailDialogController service proxy
    
    * Feature #70119, EmailDialogController service proxy, clean up

--- core/src/main/java/inetsoft/web/viewsheet/controller/dialog/EmailDialogService.java
+++ core/src/main/java/inetsoft/web/viewsheet/controller/dialog/EmailDialogService.java
@@ -168,7 +168,8 @@ public class EmailDialogService {
          .userDialogEnabled(userDialogEnabled)
          .emailAddrDialogModel(emailAddrDialogModel)
          .users(usersModel == null ? new ArrayList<>() : usersModel.emailUsers())
-         .groups(usersModel == null ? new ArrayList<>() : Arrays.asList(usersModel.groupBaseNames()))
+         .groups(usersModel == null ? new ArrayList<>() :
+                    Arrays.stream(usersModel.groups()).map(IdentityID::getName).toList())
          .emailGroups(usersModel == null ? new ArrayList<>() : usersModel.emailGroups())
          .build();
       boolean historyEnabled = "true".equalsIgnoreCase(SreeEnv.getProperty("mail.history.enabled"));
