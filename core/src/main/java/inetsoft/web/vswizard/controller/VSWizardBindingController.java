00c283836f87f56f51a097a722d3984005d748ff
Nicholas <nicholas.govus@inetsoft.com>
2025-03-24 08:30:37 -0600

    Feature #70119, VSWizardBindingController service proxy

--- core/src/main/java/inetsoft/web/vswizard/controller/VSWizardBindingController.java
+++ core/src/main/java/inetsoft/web/vswizard/controller/VSWizardBindingController.java
@@ -17,53 +17,22 @@
  */
 package inetsoft.web.vswizard.controller;
 
-import inetsoft.analytic.composition.ViewsheetService;
-import inetsoft.report.composition.AssetTreeModel;
-import inetsoft.report.composition.RuntimeViewsheet;
-import inetsoft.report.composition.execution.ViewsheetSandbox;
-import inetsoft.report.internal.Util;
-import inetsoft.uql.ColumnSelection;
-import inetsoft.uql.asset.*;
-import inetsoft.uql.erm.DataRef;
-import inetsoft.uql.viewsheet.ChartVSAssembly;
-import inetsoft.uql.viewsheet.Viewsheet;
-import inetsoft.uql.viewsheet.graph.*;
-import inetsoft.uql.viewsheet.internal.ChartVSAssemblyInfo;
-import inetsoft.uql.viewsheet.internal.VSUtil;
-import inetsoft.util.Catalog;
-import inetsoft.web.binding.command.SetGrayedOutFieldsCommand;
-import inetsoft.web.binding.command.VSTrapCommand;
-import inetsoft.web.binding.drm.DataRefModel;
-import inetsoft.web.binding.handler.VSAssemblyInfoHandler;
-import inetsoft.web.binding.handler.VSTreeHandler;
-import inetsoft.web.binding.model.ChartBindingModel;
-import inetsoft.web.composer.model.TreeNodeModel;
-import inetsoft.web.composer.model.vs.SourceChangeMessage;
+
 import inetsoft.web.viewsheet.LoadingMask;
-import inetsoft.web.viewsheet.command.MessageCommand;
 import inetsoft.web.viewsheet.model.RuntimeViewsheetRef;
 import inetsoft.web.viewsheet.service.CommandDispatcher;
 import inetsoft.web.viewsheet.service.LinkUri;
 import inetsoft.web.vswizard.HandleWizardExceptions;
 import inetsoft.web.vswizard.Recommend;
-import inetsoft.web.vswizard.command.*;
 import inetsoft.web.vswizard.event.*;
-import inetsoft.web.vswizard.handler.VSWizardBindingHandler;
-import inetsoft.web.vswizard.model.*;
-import inetsoft.web.vswizard.model.recommender.*;
-import inetsoft.web.vswizard.recommender.VSRecommendationFactory;
-import inetsoft.web.vswizard.recommender.WizardRecommenderUtil;
-import inetsoft.web.vswizard.service.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 
 import java.security.Principal;
-import java.util.*;
 
 /**
  * vs wizard binding tree controller.
