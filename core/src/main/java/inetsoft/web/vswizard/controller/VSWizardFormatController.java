6e586c9a751afd36c74e29dbee08b90d99e879a9
ngovus-inetsoft <nicholas.govus@inetsoft.com>
2025-03-24 07:50:36 -0600

    Feature #70119, VSWizardFormatController service proxy (#414)

--- core/src/main/java/inetsoft/web/vswizard/controller/VSWizardFormatController.java
+++ core/src/main/java/inetsoft/web/vswizard/controller/VSWizardFormatController.java
@@ -17,23 +17,10 @@
  */
 package inetsoft.web.vswizard.controller;
 
-import inetsoft.analytic.composition.ViewsheetService;
-import inetsoft.report.composition.RuntimeViewsheet;
-import inetsoft.report.composition.execution.ViewsheetSandbox;
-import inetsoft.uql.XConstants;
-import inetsoft.uql.viewsheet.*;
-import inetsoft.util.Tool;
-import inetsoft.web.adhoc.model.FormatInfoModel;
 import inetsoft.web.viewsheet.model.RuntimeViewsheetRef;
 import inetsoft.web.viewsheet.service.*;
 import inetsoft.web.vswizard.HandleWizardExceptions;
 import inetsoft.web.vswizard.event.SetWizardBindingFormatEvent;
-import inetsoft.web.vswizard.handler.VSWizardBindingHandler;
-import inetsoft.web.vswizard.model.recommender.VSTemporaryInfo;
-import inetsoft.web.vswizard.recommender.WizardRecommenderUtil;
-import inetsoft.web.vswizard.service.VSWizardTemporaryInfoService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -45,15 +32,11 @@ import java.security.Principal;
 public class VSWizardFormatController {
 
    @Autowired
-   public VSWizardFormatController(ViewsheetService viewsheetService,
-                                   RuntimeViewsheetRef runtimeViewsheetRef,
-                                   VSWizardBindingHandler bindingHandler,
-                                   VSWizardTemporaryInfoService temporaryInfoService)
+   public VSWizardFormatController(VSWizardFormatServiceProxy vsWizardFormatServiceProxy,
+                                   RuntimeViewsheetRef runtimeViewsheetRef)
    {
-      this.viewsheetService = viewsheetService;
       this.runtimeViewsheetRef = runtimeViewsheetRef;
-      this.bindingHandler = bindingHandler;
-      this.temporaryInfoService = temporaryInfoService;
+      this.vsWizardFormatServiceProxy = vsWizardFormatServiceProxy;
    }
 
    @HandleWizardExceptions
@@ -69,62 +52,9 @@ public class VSWizardFormatController {
          return;
       }
 
-      RuntimeViewsheet rvs = viewsheetService.getViewsheet(id, principal);
-      ViewsheetSandbox box = rvs.getViewsheetSandbox();
-      box.lockRead();
-
-      try {
-         VSTemporaryInfo tinfo = temporaryInfoService.getVSTemporaryInfo(rvs);
-         FormatInfoModel model = event.getModel();
-         VSFormat fmt = tinfo.getUserFormat(event.getField());
-
-         if(fmt == null) {
-            tinfo.setFormat(event.getField(), fmt = new VSFormat());
-         }
-
-         String modelFormat = FormatInfoModel.getDurationFormat(model.getFormat(),
-            model.isDurationPadZeros());
-         String formatSpec = model.getFormatSpec();
-         String dateSpec = model.getDateSpec();
-         FormatInfoModel oldFormat = bindingHandler.getBindingFormatModel(fmt);
-         String oldModelFormat = FormatInfoModel.getDurationFormat(oldFormat.getFormat(),
-            oldFormat.isDurationPadZeros());
-
-         if(!Tool.equals(modelFormat, oldModelFormat) ||
-            !Tool.equals(formatSpec, oldFormat.getFormatSpec()) ||
-            !Tool.equals(dateSpec, oldFormat.getDateSpec()))
-         {
-            if(modelFormat != null && modelFormat.equals(XConstants.COMMA_FORMAT)) {
-               modelFormat = XConstants.DECIMAL_FORMAT;
-               formatSpec = "#,##0";
-            }
-            else if(XConstants.DATE_FORMAT.equals(modelFormat) && !"Custom".equals(dateSpec)) {
-               formatSpec = dateSpec;
-            }
-
-            fmt.setFormatExtentValue(formatSpec);
-            fmt.setFormatValue(modelFormat);
-            VSFormat donutChartCalcFmt = tinfo.getUserFormat("Total@" + event.getField());
-
-            if(donutChartCalcFmt != null) {
-               donutChartCalcFmt.setFormatExtentValue(formatSpec);
-               donutChartCalcFmt.setFormatValue(modelFormat);
-            }
-
-            Viewsheet vs = rvs.getViewsheet();
-            VSAssembly tempAssembly = WizardRecommenderUtil.getTempAssembly(vs);
-            bindingHandler.updatePrimaryAssembly(rvs, linkUri, tempAssembly, false, false, dispatcher);
-         }
-      }
-      finally {
-         box.unlockRead();
-      }
+      vsWizardFormatServiceProxy.updateFormat(id, event, dispatcher, principal, linkUri);
    }
 
-   private final ViewsheetService viewsheetService;
    private final RuntimeViewsheetRef runtimeViewsheetRef;
-   private final VSWizardBindingHandler bindingHandler;
-   private final VSWizardTemporaryInfoService temporaryInfoService;
-
-   private static final Logger LOGGER = LoggerFactory.getLogger(VSWizardFormatController.class);
+   private VSWizardFormatServiceProxy vsWizardFormatServiceProxy;
 }
