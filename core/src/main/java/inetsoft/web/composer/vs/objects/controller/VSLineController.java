58645647a67dd5f8cd11d28fa4d86dc130785f36
ngovus-inetsoft <nicholas.govus@inetsoft.com>
2025-03-27 07:56:31 -0600

    Feature #70119, VSLineController service proxy (#485)

--- core/src/main/java/inetsoft/web/composer/vs/objects/controller/VSLineController.java
+++ core/src/main/java/inetsoft/web/composer/vs/objects/controller/VSLineController.java
@@ -17,12 +17,6 @@
  */
 package inetsoft.web.composer.vs.objects.controller;
 
-import inetsoft.analytic.composition.ViewsheetService;
-import inetsoft.report.composition.RuntimeViewsheet;
-import inetsoft.uql.viewsheet.LineVSAssembly;
-import inetsoft.uql.viewsheet.Viewsheet;
-import inetsoft.uql.viewsheet.internal.LineVSAssemblyInfo;
-import inetsoft.web.binding.handler.VSAssemblyInfoHandler;
 import inetsoft.web.composer.vs.objects.event.ResizeVSLineEvent;
 import inetsoft.web.viewsheet.LoadingMask;
 import inetsoft.web.viewsheet.Undoable;
@@ -34,12 +28,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
-
-import java.awt.*;
 import java.security.Principal;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 /**
  * Controller that processes vs line events.
@@ -51,17 +41,13 @@ public class VSLineController {
     *
     * @param runtimeViewsheetRef reference to the runtime viewsheet associated
     *                            with the WebSocket session.
-    * @param viewsheetService
     */
    @Autowired
-   public VSLineController(
-      RuntimeViewsheetRef runtimeViewsheetRef,
-      ViewsheetService viewsheetService,
-      VSAssemblyInfoHandler vsAssemblyInfoHandler)
+   public VSLineController(RuntimeViewsheetRef runtimeViewsheetRef,
+                           VSLineServiceProxy vsLineServiceProxy)
    {
       this.runtimeViewsheetRef = runtimeViewsheetRef;
-      this.viewsheetService = viewsheetService;
-      this.vsAssemblyInfoHandler = vsAssemblyInfoHandler;
+      this.vsLineServiceProxy = vsLineServiceProxy;
    }
 
    /**
@@ -79,49 +65,11 @@ public class VSLineController {
    public void resize(@Payload ResizeVSLineEvent event, Principal principal,
                       CommandDispatcher dispatcher) throws Exception
    {
-      RuntimeViewsheet rvs = viewsheetService.getViewsheet(
-         this.runtimeViewsheetRef.getRuntimeId(), principal);
-      Viewsheet viewsheet = rvs.getViewsheet();
-      LineVSAssembly assembly = (LineVSAssembly) viewsheet.getAssembly(event.getName());
-
-      assert assembly != null;
-
-      Point startPosition = new Point(event.getStartLeft(), event.getStartTop());
-      Point endPosition = new Point(event.getEndLeft(), event.getEndTop());
-      Point offset = new Point(event.getOffsetX(), event.getOffsetY());
-      Dimension size = new Dimension(Math.max(1, Math.abs(endPosition.x - startPosition.x)),
-                                     Math.max(1, Math.abs(endPosition.y - startPosition.y)));
-
-      assembly.setStartPos(startPosition);
-      assembly.setEndPos(endPosition);
-      assembly.setPixelOffset(offset);
-      assembly.setPixelSize(size);
-
-      LineVSAssemblyInfo lineInfo = (LineVSAssemblyInfo) assembly.getInfo();
-
-      if(event.getStartAnchorId() != null) {
-         lineInfo.setStartAnchorID(event.getStartAnchorId());
-         lineInfo.setStartAnchorPos(event.getStartAnchorPos());
-      }
-      else {
-         lineInfo.setStartAnchorID(null);
-      }
-
-      if(event.getEndAnchorId() != null) {
-         lineInfo.setEndAnchorID(event.getEndAnchorId());
-         lineInfo.setEndAnchorPos(event.getEndAnchorPos());
-      }
-      else {
-         lineInfo.setEndAnchorID(null);
-      }
-
-      List<String> assemblyName = Stream.of(event.getName()).collect(Collectors.toList());
-      vsAssemblyInfoHandler.updateAnchoredLines(rvs, assemblyName, dispatcher);
+      vsLineServiceProxy.resize(runtimeViewsheetRef.getRuntimeId(), event, principal, dispatcher);
    }
 
    private final RuntimeViewsheetRef runtimeViewsheetRef;
-   private final ViewsheetService viewsheetService;
-   private final VSAssemblyInfoHandler vsAssemblyInfoHandler;
+   private VSLineServiceProxy vsLineServiceProxy;
 
    private static final Logger LOG =
       LoggerFactory.getLogger(VSLineController.class);
