f6e4770f560f766a88142beb628e6ddd33fe672c
Kenneth Sandoval <kenneth.sandoval@inetsoft.com>
2025-03-27 16:11:59 -0400

    Feature #70119 Fix HeatBeatService qualifier

--- core/src/test/java/inetsoft/web/composer/controller/HeartBeatController.java
+++ core/src/test/java/inetsoft/web/composer/controller/HeartBeatController.java
@@ -42,7 +42,7 @@ public class HeartBeatController {
    }
 
    @Autowired
-   @Qualifier("worksheetService")
+   @Qualifier("heartBeatService")
    public void setHeartBeatService(HeartBeatServiceProxy heartBeatService) {
       this.heartBeatService = heartBeatService;
    }
