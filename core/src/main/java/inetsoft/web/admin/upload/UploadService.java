91a56983254b40b6461f3d174a0efb5ea7074bee
Jason Shobe <jason.shobe@inetsoft.com>
2025-03-28 13:46:16 -0400

    Make upload service distributed.

--- core/src/main/java/inetsoft/web/admin/upload/UploadService.java
+++ core/src/main/java/inetsoft/web/admin/upload/UploadService.java
@@ -17,41 +17,45 @@
  */
 package inetsoft.web.admin.upload;
 
+import inetsoft.sree.internal.cluster.Cluster;
+import inetsoft.sree.internal.cluster.DistributedMap;
+import inetsoft.storage.BlobStorage;
+import inetsoft.storage.BlobTransaction;
+import inetsoft.util.FileSystemService;
+import inetsoft.util.SingletonManager;
 import jakarta.annotation.PreDestroy;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.io.*;
+import java.nio.file.*;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 @Component
 public class UploadService {
    public UploadService(MavenClientService mavenClient) {
       this.mavenClient = mavenClient;
+      this.uploads = Cluster.getInstance().getMap(getClass().getName() + ".uploads");
    }
 
    @PreDestroy
    public void removeAll() {
-      for(String id : uploads.keySet()) {
-         remove(id);
-      }
+      new ArrayList<>(uploads.keySet()) // copy to temp list before removing
+         .forEach(this::remove);
    }
 
    @Scheduled(initialDelay = 300000L, fixedDelay = 300000L)
    public void removeExpired() {
       long limit = System.currentTimeMillis() - 300000L;
-
-      for(Map.Entry<String, Upload> e : uploads.entrySet()) {
-         if(e.getValue().accessed < limit) {
-            remove(e.getKey());
-         }
-      }
+      uploads.entrySet().stream()
+         .filter(e -> e.getValue().accessed < limit)
+         .map(Map.Entry::getKey)
+         .toList() // copy to temp list before removing
+         .forEach(this::remove);
    }
 
    public List<String> search(String query) {
@@ -76,7 +80,7 @@ public class UploadService {
       String id = UUID.randomUUID().toString();
       Upload upload = new Upload();
       upload.accessed = System.currentTimeMillis();
-      upload.files = files;
+      upload.files = files.stream().map(f -> upload(id, f)).toList();
       uploads.put(id, upload);
       return id;
    }
@@ -89,18 +93,21 @@ public class UploadService {
       }
 
       upload.accessed = System.currentTimeMillis();
-      Map<String, UploadedFile> updated = new HashMap<>();
+      Map<String, UploadFileInfo> updated = new HashMap<>();
 
-      for(UploadedFile file : upload.files) {
-         updated.put(file.fileName(), file);
+      for(UploadFileInfo file : upload.files) {
+         updated.put(file.fileName, file);
       }
 
       for(UploadedFile file : files) {
-         updated.put(file.fileName(), file);
+         updated.put(file.fileName(), upload(id, file));
       }
 
       upload.files = new ArrayList<>(updated.values());
-      return Collections.unmodifiableList(upload.files);
+      uploads.put(id, upload);
+      return upload.files.stream()
+         .map(this::download)
+         .toList();
    }
 
    public Optional<List<UploadedFile>> get(String id) {
@@ -111,30 +118,148 @@ public class UploadService {
       }
 
       upload.accessed = System.currentTimeMillis();
-      return Optional.of(upload.files);
+
+      if(upload.files == null) {
+         return Optional.empty();
+      }
+
+      return Optional.of(upload.files.stream()
+         .map(this::download)
+         .toList());
    }
 
    public void remove(String id) {
       Upload upload = uploads.remove(id);
 
       if(upload != null) {
-         for(UploadedFile file : upload.files) {
+         for(UploadFileInfo info : upload.files) {
             try {
-               Files.deleteIfExists(file.file().toPath());
+               processLocalFile(info, f -> {
+                  if(Files.exists(f)) {
+                     Files.delete(f);
+                  }
+               });
             }
             catch(IOException e) {
-               LOG.warn("Failed to delete temporary uploaded file", e);
+               LOG.warn("Failed to delete local file", e);
+            }
+
+            try {
+               blobStorage.delete(info.blob);
+            }
+            catch(Exception e) {
+               LOG.warn("Failed to delete blob", e);
             }
          }
       }
    }
 
+   private UploadFileInfo upload(String id, UploadedFile file) {
+      UploadFileInfo info = new UploadFileInfo();
+      info.fileName = file.fileName();
+      info.blob = id + "/" + file.fileName();
+
+      if(file.multipartFile() != null) {
+         FileSystemService fsService = SingletonManager.getInstance(FileSystemService.class);
+         File localFile = fsService.getCacheTempFile("upload-" + id, ".dat");
+
+         try {
+            info.filePath = fsService.getCacheFolder().toPath().toAbsolutePath()
+               .relativize(localFile.toPath().toAbsolutePath()).toString();
+
+            try(InputStream in = file.multipartFile().getInputStream();
+                BlobTransaction<Metadata> tx = blobStorage.beginTransaction();
+                OutputStream out = tx.newStream(info.blob, new Metadata()))
+            {
+               IOUtils.copy(in, out);
+               tx.commit();
+            }
+         }
+         catch(Exception e) {
+            throw new RuntimeException("Failed to upload file", e);
+         }
+      }
+      else if(file.file() != null) {
+         info.filePath = file.file().getAbsolutePath();
+
+         try(InputStream in = Files.newInputStream(file.file().toPath());
+             BlobTransaction<Metadata> tx = blobStorage.beginTransaction();
+             OutputStream out = tx.newStream(info.blob, new Metadata()))
+         {
+            IOUtils.copy(in, out);
+            tx.commit();
+         }
+         catch(Exception e) {
+            throw new RuntimeException("Failed to upload file", e);
+         }
+      }
+      else {
+         throw new IllegalStateException("Either the file or multipart file must be set");
+      }
+
+      return info;
+   }
+
+   private UploadedFile download(UploadFileInfo info) {
+      Path localFile;
+
+      try {
+         localFile = processLocalFile(info, f -> {
+            if(!Files.exists(f)) {
+               try(InputStream in = blobStorage.getInputStream(info.blob);
+                   OutputStream out = Files.newOutputStream(f))
+               {
+                  IOUtils.copy(in, out);
+               }
+            }
+         });
+      }
+      catch(IOException e) {
+         throw new RuntimeException("Failed to download file", e);
+      }
+
+      return UploadedFile.builder()
+         .fileName(info.fileName)
+         .file(localFile.toFile())
+         .build();
+   }
+
+   private Path processLocalFile(UploadFileInfo info, LocalFileProcessor processor)
+      throws IOException
+   {
+      Path localFile = Paths.get(info.filePath);
+
+      if(!localFile.isAbsolute()) {
+         localFile = FileSystemService.getInstance().getCacheFolder().toPath().resolve(localFile);
+         processor.process(localFile);
+      }
+
+      return localFile;
+   }
+
    private final MavenClientService mavenClient;
-   private final Map<String, Upload> uploads = new ConcurrentHashMap<>();
+   private final DistributedMap<String, Upload> uploads;
+   @SuppressWarnings("unchecked")
+   private final BlobStorage<Metadata> blobStorage =
+      SingletonManager.getInstance(BlobStorage.class, "fileUploads", false);
    private static final Logger LOG = LoggerFactory.getLogger(UploadService.class);
+
+   public static final class Metadata implements Serializable {
+   }
+
+   @FunctionalInterface
+   private interface LocalFileProcessor {
+      void process(Path localFile) throws IOException;
+   }
+
+   private static final class UploadFileInfo implements Serializable {
+      String fileName;
+      String filePath;
+      String blob;
+   }
 
    private static final class Upload {
       long accessed;
-      List<UploadedFile> files;
+      List<UploadFileInfo> files;
    }
 }
