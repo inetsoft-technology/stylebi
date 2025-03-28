144b64b67d721030300b1173b16ebd1aa1ad63f8
ngovus-inetsoft <nicholas.govus@inetsoft.com>
2025-03-28 07:33:56 -0600

    Feature #70119, SubmitPropertyDialogController service proxy (#530)

--- core/src/main/java/inetsoft/web/composer/vs/dialog/SubmitPropertyDialogController.java
+++ core/src/main/java/inetsoft/web/composer/vs/dialog/SubmitPropertyDialogController.java
@@ -17,14 +17,7 @@
  */
 package inetsoft.web.composer.vs.dialog;
 
-import inetsoft.analytic.composition.ViewsheetService;
-import inetsoft.report.composition.RuntimeViewsheet;
-import inetsoft.uql.viewsheet.SubmitVSAssembly;
-import inetsoft.uql.viewsheet.Viewsheet;
-import inetsoft.uql.viewsheet.internal.SubmitVSAssemblyInfo;
-import inetsoft.util.Tool;
 import inetsoft.web.composer.model.vs.*;
-import inetsoft.web.composer.vs.objects.controller.VSObjectPropertyService;
 import inetsoft.web.factory.RemainingPath;
 import inetsoft.web.viewsheet.LoadingMask;
 import inetsoft.web.viewsheet.Undoable;
@@ -34,8 +27,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.awt.*;
 import java.security.Principal;
 
 /**
@@ -47,21 +38,14 @@ import java.security.Principal;
 public class SubmitPropertyDialogController {
    /**
     * Creates a new instance of <tt>SubmitPropertyDialogController</tt>.
-    * @param vsObjectPropertyService VSObjectProperty service
     * @param runtimeViewsheetRef     RuntimeViewsheetRef service
-    * @param viewsheetService
     */
    @Autowired
-   public SubmitPropertyDialogController(
-      VSObjectPropertyService vsObjectPropertyService,
-      RuntimeViewsheetRef runtimeViewsheetRef,
-      VSDialogService dialogService,
-      ViewsheetService viewsheetService)
+   public SubmitPropertyDialogController(RuntimeViewsheetRef runtimeViewsheetRef,
+                                         SubmitPropertyDialogServiceProxy submitPropertyDialogServiceProxy)
    {
-      this.vsObjectPropertyService = vsObjectPropertyService;
       this.runtimeViewsheetRef = runtimeViewsheetRef;
-      this.dialogService = dialogService;
-      this.viewsheetService = viewsheetService;
+      this.submitPropertyDialogServiceProxy = submitPropertyDialogServiceProxy;
    }
 
    /**
@@ -82,59 +66,7 @@ public class SubmitPropertyDialogController {
                                                                  Principal principal)
       throws Exception
    {
-      RuntimeViewsheet rvs;
-      Viewsheet vs;
-      SubmitVSAssembly submitAssembly;
-      SubmitVSAssemblyInfo submitAssemblyInfo;
-
-      try {
-         rvs = viewsheetService.getViewsheet(runtimeId, principal);
-         vs = rvs.getViewsheet();
-         submitAssembly = (SubmitVSAssembly) vs.getAssembly(objectId);
-         submitAssemblyInfo = (SubmitVSAssemblyInfo) submitAssembly.getVSAssemblyInfo();
-      }
-      catch(Exception e) {
-         //TODO decide what to do with exception
-         throw e;
-      }
-
-      SubmitPropertyDialogModel result = new SubmitPropertyDialogModel();
-      SubmitGeneralPaneModel submitGeneralPaneModel = result.getSubmitGeneralPaneModel();
-      LabelPropPaneModel labelPropPaneModel = submitGeneralPaneModel.getLabelPropPaneModel();
-      GeneralPropPaneModel generalPropPaneModel = submitGeneralPaneModel.getGeneralPropPaneModel();
-      SizePositionPaneModel sizePositionPaneModel =
-         submitGeneralPaneModel.getSizePositionPaneModel();
-      BasicGeneralPaneModel basicGeneralPaneModel = generalPropPaneModel.getBasicGeneralPaneModel();
-
-      ClickableScriptPaneModel.Builder clickableScriptPaneModel =
-         ClickableScriptPaneModel.builder();
-
-      generalPropPaneModel.setShowEnabledGroup(true);
-      generalPropPaneModel.setEnabled(submitAssemblyInfo.getEnabledValue());
-
-      Point pos = dialogService.getAssemblyPosition(submitAssemblyInfo, vs);
-      Dimension size = dialogService.getAssemblySize(submitAssemblyInfo, vs);
-
-      sizePositionPaneModel.setPositions(pos, size);
-      sizePositionPaneModel.setContainer(submitAssembly.getContainer() != null);
-
-      basicGeneralPaneModel.setName(submitAssemblyInfo.getAbsoluteName());
-      basicGeneralPaneModel.setRefresh(submitAssemblyInfo.isRefresh());
-      basicGeneralPaneModel.setPrimary(submitAssemblyInfo.isPrimary());
-      basicGeneralPaneModel.setVisible(submitAssemblyInfo.getVisibleValue());
-      basicGeneralPaneModel.setObjectNames(
-         this.vsObjectPropertyService.getObjectNames(vs, submitAssemblyInfo.getAbsoluteName()));
-
-      labelPropPaneModel.setLabel(submitAssemblyInfo.getLabelName());
-
-      String script = submitAssemblyInfo.getScript() == null ? "" : submitAssemblyInfo.getScript();
-      String onClick = submitAssemblyInfo.getOnClick() == null ? "" :submitAssemblyInfo.getOnClick();
-      clickableScriptPaneModel.scriptEnabled(submitAssemblyInfo.isScriptEnabled());
-      clickableScriptPaneModel.scriptExpression(script);
-      clickableScriptPaneModel.onClickExpression(onClick);
-      result.setClickableScriptPaneModel(clickableScriptPaneModel.build());
-
-      return result;
+      return submitPropertyDialogServiceProxy.getSubmitPropertyDialogModel(runtimeId, objectId, principal);
    }
 
    /**
@@ -153,50 +85,10 @@ public class SubmitPropertyDialogController {
                                             CommandDispatcher commandDispatcher)
       throws Exception
    {
-      RuntimeViewsheet viewsheet;
-      SubmitVSAssemblyInfo submitAssemblyInfo;
-
-      try {
-         viewsheet = viewsheetService.getViewsheet(this.runtimeViewsheetRef.getRuntimeId(), principal);
-         SubmitVSAssembly submitAssembly = (SubmitVSAssembly) viewsheet.getViewsheet().getAssembly(objectId);
-         submitAssemblyInfo = (SubmitVSAssemblyInfo) Tool.clone(submitAssembly.getVSAssemblyInfo());
-      }
-      catch(Exception e) {
-         //TODO decide what to do with exception
-         throw e;
-      }
-
-      SubmitGeneralPaneModel submitGeneralPaneModel = value.getSubmitGeneralPaneModel();
-      LabelPropPaneModel labelPropPaneModel = submitGeneralPaneModel.getLabelPropPaneModel();
-      GeneralPropPaneModel generalPropPaneModel = submitGeneralPaneModel.getGeneralPropPaneModel();
-      SizePositionPaneModel sizePositionPaneModel =
-         submitGeneralPaneModel.getSizePositionPaneModel();
-      BasicGeneralPaneModel basicGeneralPaneModel = generalPropPaneModel.getBasicGeneralPaneModel();
-      ClickableScriptPaneModel clickableScriptPaneModel = value.getClickableScriptPaneModel();
-
-      submitAssemblyInfo.setEnabledValue(generalPropPaneModel.getEnabled());
-
-      dialogService.setAssemblySize(submitAssemblyInfo, sizePositionPaneModel);
-      dialogService.setAssemblyPosition(submitAssemblyInfo, sizePositionPaneModel);
-
-      submitAssemblyInfo.setRefreshValue(basicGeneralPaneModel.isRefresh() + "");
-      submitAssemblyInfo.setPrimary(basicGeneralPaneModel.isPrimary());
-      submitAssemblyInfo.setVisibleValue(basicGeneralPaneModel.getVisible());
-
-      submitAssemblyInfo.setLabelName(labelPropPaneModel.getLabel());
-
-      submitAssemblyInfo.setScriptEnabled(clickableScriptPaneModel.scriptEnabled());
-
-      submitAssemblyInfo.setOnClick(clickableScriptPaneModel.onClickExpression() == null ? "" : clickableScriptPaneModel.onClickExpression());
-      submitAssemblyInfo.setScript(clickableScriptPaneModel.scriptExpression() == null ? "" : clickableScriptPaneModel.scriptExpression());
-
-      this.vsObjectPropertyService.editObjectProperty(
-         viewsheet, submitAssemblyInfo, objectId, basicGeneralPaneModel.getName(), linkUri,
-         principal, commandDispatcher);
+      submitPropertyDialogServiceProxy.setSubmitPropertyDialogModel(runtimeViewsheetRef.getRuntimeId(),
+                                                                    objectId, value, linkUri, principal, commandDispatcher);
    }
 
-   private final VSObjectPropertyService vsObjectPropertyService;
    private final RuntimeViewsheetRef runtimeViewsheetRef;
-   private final VSDialogService dialogService;
-   private final ViewsheetService viewsheetService;
+   private final SubmitPropertyDialogServiceProxy submitPropertyDialogServiceProxy;
 }
