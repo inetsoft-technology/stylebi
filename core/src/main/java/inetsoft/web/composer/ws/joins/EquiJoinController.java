f7f418df75681f618ac482581a5b37b2be8c3253
Nicholas <nicholas.govus@inetsoft.com>
2025-04-01 15:45:33 -0600

    Feature #70119, EquiJoinController service proxy

--- core/src/main/java/inetsoft/web/composer/ws/joins/EquiJoinController.java
+++ core/src/main/java/inetsoft/web/composer/ws/joins/EquiJoinController.java
@@ -17,12 +17,7 @@
  */
 package inetsoft.web.composer.ws.joins;
 
-import inetsoft.report.composition.RuntimeWorksheet;
-import inetsoft.report.composition.event.AssetEventUtil;
-import inetsoft.uql.XConstants;
-import inetsoft.uql.asset.*;
 import inetsoft.web.composer.ws.WorksheetController;
-import inetsoft.web.composer.ws.assembly.WorksheetEventUtil;
 import inetsoft.web.composer.ws.event.WSEquiJoinEvent;
 import inetsoft.web.viewsheet.LoadingMask;
 import inetsoft.web.viewsheet.Undoable;
@@ -30,9 +25,7 @@ import inetsoft.web.viewsheet.service.CommandDispatcher;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
-
 import java.security.Principal;
-import java.util.Enumeration;
 
 /**
  * Controller for toggling the equijoin state of operators in a
@@ -40,6 +33,12 @@ import java.util.Enumeration;
  */
 @Controller
 public class EquiJoinController extends WorksheetController {
+
+   public EquiJoinController(EquiJoinServiceProxy equiJoinServiceProxy)
+   {
+      this.equiJoinServiceProxy = equiJoinServiceProxy;
+   }
+
    /**
     * Sets the equijoin state of a pair of subtables in a RelationalJoinTableAssembly.
     */
@@ -50,42 +49,8 @@ public class EquiJoinController extends WorksheetController {
       @Payload WSEquiJoinEvent event, Principal principal,
       CommandDispatcher commandDispatcher) throws Exception
    {
-      RuntimeWorksheet rws = super.getRuntimeWorksheet(principal);
-      Worksheet ws = rws.getWorksheet();
-      RelationalJoinTableAssembly joinTable =
-         (RelationalJoinTableAssembly) ws.getAssembly(event.joinTable());
-      Enumeration operatorTables = joinTable.getOperatorTables();
-
-      while(operatorTables.hasMoreElements()) {
-         String[] pair = (String[]) operatorTables.nextElement();
-         String leftTable = pair[0];
-         String rightTable = pair[1];
-         TableAssemblyOperator tableOperator = joinTable.getOperator(leftTable, rightTable);
-
-         for(TableAssemblyOperator.Operator operator : tableOperator.getOperators()) {
-            if(!event.leftSubtable().equals(leftTable) ||
-               !event.rightSubtable().equals(rightTable))
-            {
-               continue;
-            }
-
-            if(operator.getOperation() == XConstants.INNER_JOIN ||
-               operator.getOperation() == XConstants.LEFT_JOIN ||
-               operator.getOperation() == XConstants.RIGHT_JOIN ||
-               operator.getOperation() == XConstants.FULL_JOIN)
-            {
-               operator.setOperation(event.operation());
-            }
-         }
-      }
-
-      WorksheetEventUtil.refreshColumnSelection(
-         rws, joinTable.getName(), false);
-      WorksheetEventUtil.loadTableData(
-         rws, joinTable.getAbsoluteName(), true, true);
-      WorksheetEventUtil.refreshAssembly(
-         rws, joinTable.getAbsoluteName(), true, commandDispatcher, principal);
-      AssetEventUtil.refreshTableLastModified(
-         ws, joinTable.getAbsoluteName(), true);
+      equiJoinServiceProxy.setEquiJoin(super.getRuntimeId(), event, principal, commandDispatcher);
    }
+
+   private final EquiJoinServiceProxy equiJoinServiceProxy;
 }
