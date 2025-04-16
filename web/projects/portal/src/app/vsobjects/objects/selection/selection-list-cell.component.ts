b318d87d39973bbc3367dd6e4e1e691d5cbb6c15
nelson <nelsoncao@inetsoft.com>
2025-04-16 09:46:21 +0800

    fix Bug #70800, increase mobile selection list/selection tree icon size for max mode.

--- web/projects/portal/src/app/vsobjects/objects/selection/selection-list-cell.component.ts
+++ web/projects/portal/src/app/vsobjects/objects/selection/selection-list-cell.component.ts
@@ -64,6 +64,7 @@ export class SelectionListCell implements OnInit, OnChanges {
    @Input() isEmbedded: boolean;
    @Input() scrollbarWidth: number;
    @Input() keyNav: boolean = false;
+   @Input() maxMode: boolean = false;
    @Output() selectionStateChanged = new EventEmitter<any>();
    @Output() resizeCell = new EventEmitter<number>();
    @Output() resizeMeasures = new EventEmitter<{text: number, bar: number}>();
@@ -103,6 +104,7 @@ export class SelectionListCell implements OnInit, OnChanges {
    measureTextVAlign: string;
    isParentIDTree: boolean = false;
    htmlLabel: SafeHtml;
+   mobile = GuiTool.isMobileDevice();
 
    constructor(public vsSelectionComponent: VSSelection,
                private sanitization: DomSanitizer,
