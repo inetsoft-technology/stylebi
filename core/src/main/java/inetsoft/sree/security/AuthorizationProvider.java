a1b4ca8045e9be868e85abdb593914aa4d52a246
fredaan <fredaan@inetsoft.com>
2025-04-23 10:14:10 +0800

    fix Bug #70916, The bug occurs because resources with identical paths across different organizations share a single permission object, causing permission changes for one org to affect others. Two solutions were considered:
    1. Adjust permission-setting/deletion logic everywhere.
    
    2. Include the org ID in the resource key for isolation.
    
    Though option 2 involves more interface changes, it’s more maintainable long-term, so I proceeded with it.

--- core/src/main/java/inetsoft/sree/security/AuthorizationProvider.java
+++ core/src/main/java/inetsoft/sree/security/AuthorizationProvider.java
@@ -17,7 +17,9 @@
  */
 package inetsoft.sree.security;
 
-import inetsoft.util.Tuple3;
+import inetsoft.sree.internal.SUtil;
+import inetsoft.util.Tool;
+import inetsoft.util.Tuple4;
 
 import java.util.List;
 
@@ -38,7 +40,18 @@ public interface AuthorizationProvider
     * @param resource the resource name, such as a replet register name or a saved report path.
     * @param perm     the granted permissions.
     */
-   void setPermission(ResourceType type, String resource, Permission perm);
+   default void setPermission(ResourceType type, String resource, Permission perm) {
+      setPermission(type, resource, perm, null);
+   }
+
+   /**
+    * Sets the permissions for the specified resource.
+    *
+    * @param type     the type of resource.
+    * @param resource the resource name, such as a replet register name or a saved report path.
+    * @param perm     the granted permissions.
+    */
+   void setPermission(ResourceType type, String resource, Permission perm, String orgID);
 
    /**
     * Sets the permissions for the specified identity.
@@ -47,7 +60,18 @@ public interface AuthorizationProvider
     * @param identityID the identity id to set permissions for
     * @param perm       the granted permissions.
     */
-   void setPermission(ResourceType type, IdentityID identityID, Permission perm);
+   default void setPermission(ResourceType type, IdentityID identityID, Permission perm) {
+      setPermission(type, identityID, perm, null);
+   }
+
+   /**
+    * Sets the permissions for the specified identity.
+    *
+    * @param type       the type of resource.
+    * @param identityID the identity id to set permissions for
+    * @param perm       the granted permissions.
+    */
+   void setPermission(ResourceType type, IdentityID identityID, Permission perm, String orgID);
 
    /**
     * Clears all permissions on the specified resource.
@@ -55,7 +79,17 @@ public interface AuthorizationProvider
     * @param type     the resource type.
     * @param resource the resource name, such as a replet path or a saved report path.
     */
-   void removePermission(ResourceType type, String resource);
+   default void removePermission(ResourceType type, String resource) {
+      removePermission(type, resource, null);
+   }
+
+   /**
+    * Clears all permissions on the specified resource.
+    *
+    * @param type     the resource type.
+    * @param resource the resource name, such as a replet path or a saved report path.
+    */
+   void removePermission(ResourceType type, String resource, String orgID);
 
    /**
     * Clears all permissions on the specified identity.
@@ -63,7 +97,17 @@ public interface AuthorizationProvider
     * @param type     the resource type.
     * @param identityID the identity, such as a specified user or role.
     */
-   void removePermission(ResourceType type, IdentityID identityID);
+   default void removePermission(ResourceType type, IdentityID identityID) {
+      removePermission(type, identityID, null);
+   }
+
+   /**
+    * Clears all permissions on the specified identity.
+    *
+    * @param type     the resource type.
+    * @param identityID the identity, such as a specified user or role.
+    */
+   void removePermission(ResourceType type, IdentityID identityID, String orgID);
 
    /**
     * Gets the permissions for the specified resource.
@@ -73,7 +117,11 @@ public interface AuthorizationProvider
     *
     * @return the granted permissions or {@code null} of none have been set.
     */
-   Permission getPermission(ResourceType type, String resource);
+   default Permission getPermission(ResourceType type, String resource) {
+      return getPermission(type, resource, null);
+   }
+
+   Permission getPermission(ResourceType type, String resource, String orgID);
 
    /**
     * Gets the permissions for the specified identity.
@@ -83,14 +131,26 @@ public interface AuthorizationProvider
     *
     * @return the granted permissions or {@code null} of none have been set.
     */
-   Permission getPermission(ResourceType type, IdentityID identityID);
+   default Permission getPermission(ResourceType type, IdentityID identityID) {
+      return getPermission(type, identityID, null);
+   }
+
+   /**
+    * Gets the permissions for the specified identity.
+    *
+    * @param type       the resource type.
+    * @param identityID the identity id.
+    *
+    * @return the granted permissions or {@code null} of none have been set.
+    */
+   Permission getPermission(ResourceType type, IdentityID identityID, String orgID);
 
    /**
     * Gets the permissions for all resources
     *
     * @return the list of tuples corresponding to the resource name, their types, and their permissions
     */
-   default List<Tuple3<ResourceType, String, Permission>> getPermissions() {
+   default List<Tuple4<ResourceType, String, String, Permission>> getPermissions() {
       throw new UnsupportedOperationException();
    }
 
@@ -103,4 +163,15 @@ public interface AuthorizationProvider
     * Tear down the security provider.
     */
    void tearDown();
+
+   default String getResourceOrgID(String orgID) {
+      if(!Tool.isEmptyString(orgID)) {
+         return orgID;
+      }
+
+      // still use default orgID when multi tenant is false
+      // to keep permission not changed after switching to multi tenant.
+      return SUtil.isMultiTenant() ?
+         OrganizationManager.getInstance().getCurrentOrgID() : Organization.getDefaultOrganizationID();
+   }
 }
