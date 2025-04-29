26f2ea26a28ff229f6354122fb81e26f2e173a5d
Catherine Li <catherineli@inetsoft.com>
2025-04-29 08:55:35 +0800

    Update DependencyTransformer.java
    Using stringbuilder to merge strings in transform

--- core/src/main/java/inetsoft/uql/asset/sync/DependencyTransformer.java
+++ core/src/main/java/inetsoft/uql/asset/sync/DependencyTransformer.java
@@ -1505,8 +1505,18 @@ public abstract class DependencyTransformer {
             continue;
          }
 
-         String nChildPath = binfo.getNewName() + XUtil.DATAMODEL_PATH_SPLITER + extendModelName;
-         String oChildPath = binfo.getOldName() + XUtil.DATAMODEL_PATH_SPLITER + extendModelName;
+         StringBuilder nChildPathBuilder = new StringBuilder();
+         nChildPathBuilder.append(binfo.getNewName())
+                          .append(XUtil.DATAMODEL_PATH_SPLITER)
+                          .append(extendModelName);
+         String nChildPath = nChildPathBuilder.toString();
+
+         StringBuilder oChildPathBuilder = new StringBuilder();
+         oChildPathBuilder.append(binfo.getOldName())
+                 .append(XUtil.DATAMODEL_PATH_SPLITER)
+                 .append(extendModelName);
+         String oChildPath = oChildPathBuilder.toString();
+         
          RenameInfo rinfo = new RenameInfo(oChildPath, nChildPath, binfo.type);
          rinfo.setModelFolder(model.getFolder());
          rinfos.add(rinfo);
