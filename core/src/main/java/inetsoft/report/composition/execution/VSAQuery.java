28d8514c64afd7d096dab32c80ce3a2b46b93ad4
catherineli <catherineli@inetsoft.com>
2025-05-08 17:06:20 +0800

    fix Bug #71112: The bug is caused by in embed vs, calc use assembly binding such as "viewsheet1.crosstab1", when execute calc in next layer vs(old vs as embed vs), then the assembly binding should add calc's parent on the before("viewsheet1.viewsheet1.crosstab"

--- core/src/main/java/inetsoft/report/composition/execution/VSAQuery.java
+++ core/src/main/java/inetsoft/report/composition/execution/VSAQuery.java
@@ -1723,7 +1723,18 @@ public abstract class VSAQuery {
       TableLens lens = box.getTableData(assemblyName);
 
       if(lens == null) {
-         return null;
+         String baseAssembly = getAssembly().getAbsoluteName();
+
+         if(assemblyName.startsWith(Assembly.TABLE_VS_BOUND) || baseAssembly.contains(".")) {
+            int idx = baseAssembly.lastIndexOf(".");
+            String baseParent = baseAssembly.substring(0, idx + 1);
+            assemblyName = Assembly.TABLE_VS_BOUND + baseParent + assemblyName.substring(15);
+            lens = box.getTableData(assemblyName);
+         }
+
+         if(lens == null) {
+            return null;
+         }
       }
 
       // meta data doesn't require all rows, which would cause conversion to calc problem
