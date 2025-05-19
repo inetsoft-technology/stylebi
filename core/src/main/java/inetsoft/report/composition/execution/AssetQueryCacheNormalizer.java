2e5ee23b73312d4f5d854f8ef13763165ab50e76
Kenneth Sandoval <kenneth.sandoval@inetsoft.com>
2025-05-19 16:05:01 -0400

    Bug #71191  Code Review. Move Pattern.compile out of loop

--- core/src/main/java/inetsoft/report/composition/execution/AssetQueryCacheNormalizer.java
+++ core/src/main/java/inetsoft/report/composition/execution/AssetQueryCacheNormalizer.java
@@ -318,6 +318,8 @@ public class AssetQueryCacheNormalizer {
          }
       }
 
+      Pattern pattern = Pattern.compile(table.getName() + "\\[[\"|']" + alias + "(@.*)?]");
+
       // check if the alias is referenced in other assemblies
       for(Assembly assembly : box.getWorksheet().getAssemblies()) {
          if(!(assembly instanceof TableAssembly) || assembly == table) {
@@ -333,8 +335,6 @@ public class AssetQueryCacheNormalizer {
                continue;
             }
 
-            Pattern pattern = Pattern.compile(table.getName() + "\\[[\"|']" + alias +
-                                                 "(@.*)?]"); // digits
             ExpressionRef ref = (ExpressionRef) col.getDataRef();
             Matcher matcher = pattern.matcher(ref.getExpression());
 
