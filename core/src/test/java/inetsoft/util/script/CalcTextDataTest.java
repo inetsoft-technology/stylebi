8ea6070802a85d816d43876d49236052547aafca
ngovus-inetsoft <nicholas.govus@inetsoft.com>
2025-06-09 11:36:41 -0400

    Fix failing CalcTextDataTest, millisecond rounding to a double adds returns one more day (#1320)

--- core/src/test/java/inetsoft/util/script/CalcTextDataTest.java
+++ core/src/test/java/inetsoft/util/script/CalcTextDataTest.java
@@ -395,7 +395,7 @@ class CalcTextDataTest {
    @Test
    void testValue() {
       // Test valid date string
-      assertEquals(44925.0, CalcTextData.value("01/01/2023"), 0.01);
+      assertEquals(44926.0, CalcTextData.value("01/01/2023"), 0.01);
 
       // Test valid time string
       assertEquals(0.5, CalcTextData.value("12:00:00"), 0.01);
