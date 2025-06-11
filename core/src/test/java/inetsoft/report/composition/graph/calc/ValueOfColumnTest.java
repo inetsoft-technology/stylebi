fcef0bd99ec76868eae0a83902187050c7d2fc97
bonnieshi <bonnieshi@inetsoft.com>
2025-06-11 10:26:12 +0800

    add ValueOfColumnTest

--- core/src/test/java/inetsoft/report/composition/graph/calc/ValueOfColumnTest.java
+++ core/src/test/java/inetsoft/report/composition/graph/calc/ValueOfColumnTest.java
@@ -18,14 +18,17 @@
 
 package inetsoft.report.composition.graph.calc;
 
-import inetsoft.report.composition.graph.BrushDataSet;
-import inetsoft.report.composition.graph.VSDataSet;
+import inetsoft.graph.data.CalcColumn;
+import inetsoft.report.composition.graph.*;
 import inetsoft.report.filter.*;
 import inetsoft.report.lens.DefaultTableLens;
-import inetsoft.uql.viewsheet.VSDataRef;
-import inetsoft.uql.viewsheet.VSDimensionRef;
+import inetsoft.uql.XConstants;
+import inetsoft.uql.viewsheet.*;
+import inetsoft.uql.viewsheet.internal.DatePeriod;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -37,30 +40,31 @@ public class ValueOfColumnTest {
    private VSDataSet vsDataSet;
 
    @Test
-   void testSetGet() {
-      valueOfColumn = new ValueOfColumn("col1", "col1");
-
-      assertEquals(ValueOfCalc.FIRST, valueOfColumn.getChangeType());
-      valueOfColumn.setChangeType(ValueOfCalc.LAST);
-      assertEquals(ValueOfCalc.LAST, valueOfColumn.getChangeType());
-
-      valueOfColumn.setDim("dim1");
-      assertEquals("dim1", valueOfColumn.getDim());
-   }
-
-   @Test
    void testCalculateWithDataSet() {
       valueOfColumn = new ValueOfColumn("id", "sum(id)");
       // brush is false, return row value
       valueOfColumn.setChangeType(ValueOfCalc.LAST);
       valueOfColumn.setDim("name");
-      vsDataSet = createVSDataSet();
+
+      DefaultTableLens tb1 = new DefaultTableLens ( new Object[][]{
+         { "name", "id"},
+         { "a", 4},
+         { "b", 3},
+         { "b", 1},
+         { "c", 2}
+      });
+
+      vsDataSet = createVSDataSet(tb1, "name");
       Object result = valueOfColumn.calculate(vsDataSet, 1, false, false);
       assertEquals(2, result);
 
       valueOfColumn.setChangeType(ValueOfCalc.PREVIOUS);
       result = valueOfColumn.calculate(vsDataSet, 2, false, false);
       assertEquals(4, result);
+
+      valueOfColumn.setChangeType(ValueOfCalc.NEXT);
+      result = valueOfColumn.calculate(vsDataSet, 2, false, false);
+      assertEquals(2, result);
 
       //check brush base dataset, return the frist value, note
       valueOfColumn.setChangeType(ValueOfCalc.FIRST);
@@ -69,13 +73,63 @@ public class ValueOfColumnTest {
       assertEquals(4, result);
    }
 
-   //check return null, invalid, and BrushDataSet.ALL_HEADER_PREFIX, todo
+   @Test
+   void testCalculateWithDataSetOfPreviousYear() {
+      valueOfColumn = new ValueOfColumn("id", "sum(id)");
+      valueOfColumn.setChangeType(ValueOfCalc.PREVIOUS_YEAR);
+      valueOfColumn.setDim("date");
+      DefaultTableLens tb = new DefaultTableLens ( new Object[][]{
+         { "date", "id"},
+         { new Date(2021-1900, 0, 0), 4},
+         { new Date(2022-1900, 0, 0), 3}
+      });
+
+      vsDataSet = createVSDataSet(tb, "date");
+
+      Object result = valueOfColumn.calculate(vsDataSet, 0, false, false);
+      assertEquals(CalcColumn.INVALID, result);  // 4.9E-324 is the default value for Date, which is null. the previous year of 2021-01-01 is null
+
+      result = valueOfColumn.calculate(vsDataSet, 1, true, false);
+      assertEquals(4, result);  // the previous year of 2022-01-01 is 2021-01-01, which value is 4
+   }
 
    /**
-    * check calculate with CrossTabFilter.CrosstabDataContext, unfinished
+    * check previous range of a dc values
     */
    @Test
-   void testCalculateWithCrosstabCell() {
+   void testCalculateWithDataSetOfPreviousRange() {
+      valueOfColumn = new ValueOfColumn("id", "sum(id)");
+      valueOfColumn.setChangeType(ValueOfCalc.PREVIOUS_RANGE);
+      valueOfColumn.setDim("dateRange");
+
+      DefaultTableLens tb3 = new DefaultTableLens ( new Object[][]{
+         { "dateRange", "id"},
+         { "2019:2020", 4},
+         { "2021:2022", 3},
+         { "2023:2024", 1}
+      });
+
+      vsDataSet = createVSDataSet(tb3, "dateRange");
+
+      Object result = valueOfColumn.calculate(vsDataSet, 1, true, false);
+      assertEquals(CalcColumn.INVALID, result);  // dcPeriods is null, return INVALID
+      List<DatePeriod> datePeriods = Arrays.asList(
+         new DatePeriod(toDate("2019-01-01"), toDate("2020-01-01")),
+         new DatePeriod(toDate("2021-01-01"), toDate("2022-01-01")),
+         new DatePeriod(toDate("2023-01-01"), toDate("2024-01-01"))
+      );
+
+      valueOfColumn.setDcPeriods(datePeriods);
+      result = valueOfColumn.calculate(vsDataSet, 1, true, false);
+
+      assertEquals(4, result);  //check dcPeriods not null
+   }
+
+   /**
+    * check calculate with CrossTabFilter.CrosstabDataContext
+    */
+   @Test
+   void testCalculateWithCrosstabCellOfStaticValue() {
       valueOfColumn = new ValueOfColumn("id", "sum(id)");
 
       CrossTabFilter.CrosstabDataContext mockContext =
@@ -83,8 +137,7 @@ public class ValueOfColumnTest {
       when(mockContext.getRowHeaders()).thenReturn(List.of(new String[]{"name", "sum(id)"}));
 
       CrossFilter.Tuple rowTuple = new CrossFilter.Tuple(new Object[] { "a" });
-      CrossFilter.Tuple colTuple = new CrossFilter.Tuple(new Object[] { null });
-      CrossTabFilter.PairN pairN = new CrossTabFilter.PairN(rowTuple, colTuple, 0);
+      CrossTabFilter.PairN pairN = createCrosstabFilterPairN("a", null);
 
       valueOfColumn.setDim("name");
       valueOfColumn.setChangeType(ValueOfCalc.FIRST);
@@ -93,25 +146,187 @@ public class ValueOfColumnTest {
 
       Object result = valueOfColumn.calculate(mockContext, pairN);
       assertEquals(0.0, result);
-
-      valueOfColumn.setChangeType(ValueOfCalc.PREVIOUS);
-      result = valueOfColumn.calculate(mockContext, pairN);
-      assertEquals(4.9E-324, result);
    }
 
-   private VSDataSet createVSDataSet() {
+   /**
+    * check calculate with crosstabfilter on PREVIOUS_MONTH
+    */
+   @Test
+   void testCalculateWithCrosstabFilterOfDynamicValue() {
+      valueOfColumn = new ValueOfColumn("id", "sum(id)");
+
+      CrossTabFilter.CrosstabDataContext mockContext =
+         mock(CrossTabFilter.CrosstabDataContext.class);
+      when(mockContext.getRowHeaders()).thenReturn(List.of(new String[]{"date1", "sum(id)"}));
+
+      valueOfColumn.setDim("date1");
+      valueOfColumn.setChangeType(ValueOfCalc.PREVIOUS_MONTH);
+
+      List<Object> values = Arrays.asList(
+         toDate("2020-12-01"),
+         toDate("2021-01-01"),
+         toDate("2022-01-01"));
+
+      CrossTabFilter.PairN pairN = createCrosstabFilterPairN(toDate("2021-01-01") , null);
+      CrossTabFilter.PairN npairN = createCrosstabFilterPairN(toDate("2020-12-01"), null);
+      CrossFilter.Tuple rowTuple = new CrossFilter.Tuple(new Object[] { toDate("2021-01-01") });
+
+      when(mockContext.getValues(rowTuple, "", 0,true)).thenReturn(values);
+      when(mockContext.isPairExist(npairN)).thenReturn(true);
+      when(mockContext.getValue(npairN)).thenReturn(2);
+
+      Object result = valueOfColumn.calculate(mockContext, pairN);
+      assertEquals(2, result);
+   }
+
+   @Test
+   void testCalculateWithCrosstabFilterOfDC() {
+      valueOfColumn = new ValueOfColumn("id", "sum(id)");
+
+      CrossTabFilter.CrosstabDataContext mockContext =
+         mock(CrossTabFilter.CrosstabDataContext.class);
+      when(mockContext.getRowHeaders()).thenReturn(List.of(new String[]{"date2", "sum(id)"}));
+
+      valueOfColumn.setDim("date2");
+      valueOfColumn.setChangeType(ValueOfCalc.PREVIOUS_RANGE);
+
+      List<DatePeriod> datePeriods = Arrays.asList(
+         new DatePeriod(toDate("2020-01-01"), toDate("2021-01-01")),
+         new DatePeriod(toDate("2022-01-01"), toDate("2023-01-01")),
+         new DatePeriod(toDate("2024-01-01"), toDate("2025-01-01"))
+      );
+      valueOfColumn.setDcPeriods(datePeriods);
+
+      List<Object> values = Arrays.asList(
+         "2020:2021", "2022:2023", "2024:2025");
+      CrossFilter.Tuple rowTuple = new CrossFilter.Tuple(new Object[] {  "2022:2023" });
+
+      CrossTabFilter.PairN pairN2 = createCrosstabFilterPairN("2020:2021", null);
+      when(mockContext.getValues(rowTuple, "", 0, true)).thenReturn(values);
+      when(mockContext.isPairExist(pairN2)).thenReturn(true);
+      when(mockContext.getValue(pairN2)).thenReturn(12);
+
+      CrossTabFilter.PairN pairN = createCrosstabFilterPairN("2022:2023", null);
+      Object result = valueOfColumn.calculate(mockContext, pairN);
+
+      assertEquals(12, result);
+   }
+
+   /**
+    * check some basic functions
+    */
+   @Test
+   void testSetGetFunctions() {
+      valueOfColumn = new ValueOfColumn("col1", "col1");
+
+      assertEquals(ValueOfCalc.FIRST, valueOfColumn.getChangeType());
+      valueOfColumn.setChangeType(ValueOfCalc.LAST);
+      assertEquals(ValueOfCalc.LAST, valueOfColumn.getChangeType());
+
+      valueOfColumn.setDim("dim1");
+      assertEquals("dim1", valueOfColumn.getDim());
+
+      VSDimensionRef vsDimensionRef = mock(VSDimensionRef.class);
+      when(vsDimensionRef.isDcRange()).thenReturn(true);
+      when(vsDimensionRef.getName()).thenReturn("date2");
+      valueOfColumn.setDateComparisonDims(Arrays.asList(vsDimensionRef));
+      assertEquals("date2", valueOfColumn.getDCRangePeriodDim().getName());
+
+      valueOfColumn.setDcTempGroups(Arrays.asList(vsDimensionRef));
+      assertEquals("date2", valueOfColumn.getDcTempGroups().getFirst().getName());
+
+      valueOfColumn.setFirstWeek(true);
+   }
+
+   /**
+    * use dataset to check week groups, check previous week
+    */
+   @Test
+   void testCalculateWithDatasetOfDateWeekGroup1() {
+      valueOfColumn = new ValueOfColumn("id", "sum(id)");
+      valueOfColumn.setChangeType(ValueOfCalc.PREVIOUS_WEEK);
+      valueOfColumn.setDim("date");
+
+      DefaultTableLens tb1 = new DefaultTableLens ( new Object[][]{
+         { "date", "id"},
+         { toDate("2025-06-03"), 4},
+         { toDate("2025-06-10"), 3}
+      });
+
+      vsDataSet = createVSDataSet(tb1, "date");
+      Object result = valueOfColumn.calculate(vsDataSet, 1, false, false);
+
+      assertEquals(4, result);
+   }
+
+   @Test
+   void testCalculateWithDatesetOnAllLevel() {
+      Object result;
+
+      //check PREVIOUS_WEEK
+      DefaultTableLens tb1 = new DefaultTableLens ( new Object[][]{
+         { "date", "id"},
+         { toDate("2024-06-11"), 2},
+         { toDate("2025-06-10"), 7}
+      });
+      result = testCalculateWithDatasetOfWeekGroup(tb1, ValueOfCalc.PREVIOUS_YEAR, false);
+      assertEquals(2, result);
+
+      //check PREVIOUS_QUARTER
+      DefaultTableLens tb2 = new DefaultTableLens ( new Object[][]{
+         { "date", "id"},
+         { toDate("2025-04-27"), 8},
+         { toDate("2025-08-01"), 3}
+      });
+      result = testCalculateWithDatasetOfWeekGroup(tb2, ValueOfCalc.PREVIOUS_QUARTER, true);
+      assertEquals(8, result);
+
+      //check PREVIOUS_MONTH
+      DefaultTableLens tb3 = new DefaultTableLens ( new Object[][]{
+         { "date", "id"},
+         { toDate("2025-05-04"), 6},  // previous:05-04
+         { toDate("2025-06-01"), 3}
+      });
+      result = testCalculateWithDatasetOfWeekGroup(tb3, ValueOfCalc.PREVIOUS_MONTH, true);
+      assertEquals(6, result);
+   }
+
+   private Object testCalculateWithDatasetOfWeekGroup(DefaultTableLens tableLens, int level, Boolean isFirstWeek) {
+      valueOfColumn = new ValueOfColumn("id", "sum(id)");
+      valueOfColumn.setDim("date");
+
+      vsDataSet = createVSDataSet(tableLens, "date");
+      valueOfColumn.setChangeType(level);
+
+      VSDimensionRef vsDimensionRef = mock(VSDimensionRef.class);
+      when(vsDimensionRef.getName()).thenReturn("date");
+      when(vsDimensionRef.getDateLevel()).thenReturn(XConstants.WEEK_DATE_GROUP);
+
+      valueOfColumn.setDateComparisonDims(Arrays.asList(vsDimensionRef));
+      valueOfColumn.setFirstWeek(isFirstWeek);
+
+      return valueOfColumn.calculate(vsDataSet, 1, false, false);
+   }
+
+   private VSDataSet createVSDataSet(DefaultTableLens tableLens, String name) {
       VSDimensionRef mockDRef = mock(VSDimensionRef.class);
-      when(mockDRef.getFullName()).thenReturn("name");
-      vsDataSet = new VSDataSet(tb1, new VSDataRef[] { mockDRef });
+      when(mockDRef.getFullName()).thenReturn(name);
+      vsDataSet = new VSDataSet(tableLens, new VSDataRef[] { mockDRef });
 
       return vsDataSet;
    }
 
-   DefaultTableLens tb1 = new DefaultTableLens ( new Object[][]{
-      { "name", "id", "num" },
-      { "a", 4, 5.0 },
-      { "b", 3, 10.0 },
-      { "b", 1, 2.5 },
-      { "c", 2, 3.0 }
-   });
+   private CrossTabFilter.PairN createCrosstabFilterPairN(Object rowValue, Object colValue) {
+      CrossFilter.Tuple rowTuple = new CrossFilter.Tuple(new Object[] { rowValue });
+      CrossFilter.Tuple colTuple = new CrossFilter.Tuple(new Object[] { colValue });
+      CrossTabFilter.PairN pairN = new CrossTabFilter.PairN(rowTuple, colTuple, 0);
+
+      return pairN;
+   }
+   private java.util.Date toDate(String localDate) {
+      ZoneId zoneId = ZoneId.systemDefault();
+      return java.util.Date.from(LocalDate.parse(localDate)
+                                    .atStartOfDay(zoneId)
+                                    .toInstant());
+   }
 }
