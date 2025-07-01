800a443b083197090667285ff350fb3f67976cb3
beauzhu <beauzhu@inetsoft.com>
2025-07-01 11:00:00 +0800

    Fix Bug #71634: Disable the modification of button attributes via F12 by implementing backend permission checks to create users.

--- core/src/main/java/inetsoft/web/admin/security/user/UserTreeService.java
+++ core/src/main/java/inetsoft/web/admin/security/user/UserTreeService.java
@@ -656,6 +656,14 @@ public class UserTreeService {
     * Create a new user
     */
    EditUserPaneModel createUser(String providerName, String parentGroup, Principal principal) {
+      SecurityProvider securityProvider = SecurityEngine.getSecurity().getSecurityProvider();
+
+      if(!securityProvider.checkPermission(principal, ResourceType.SECURITY_USER,
+            "*", ResourceAction.ADMIN))
+      {
+         return null;
+      }
+
       ActionRecord actionRecord = SUtil.getActionRecord(
          principal, ActionRecord.ACTION_NAME_CREATE, null,
          ActionRecord.OBJECT_TYPE_USERPERMISSION);
