3a7d9aa4ad1e40db1efa4bf958b53b8f99b33894
frankypaninetsoft <franky.pan@inetsoft.com>
2025-07-07 15:42:18 -0400

    Feature #71805 Add databricks and clickhouse data source listing

---
+++ core/src/main/java/inetsoft/uql/listing/DatabricksDataSourceListing.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of StyleBI.
+ * Copyright (C) 2025  InetSoft Technology
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package inetsoft.uql.listing;
+
+import inetsoft.uql.DataSourceListing;
+import inetsoft.uql.XDataSource;
+import inetsoft.uql.jdbc.JDBCDataSource;
+
+public class DatabricksDataSourceListing extends DataSourceListing {
+   public DatabricksDataSourceListing() {
+      super("Databricks", "Relational Database", "/inetsoft/uql/listing/databricks.svg");
+   }
+
+   @Override
+   public XDataSource createDataSource() throws Exception {
+      final JDBCDataSource ds = new JDBCDataSource();
+      ds.setName(getAvailableName());
+      ds.setURL("jdbc:databricks://<server-hostname>:443;httpPath=<http-path>;AuthMech=3;UID=token;PWD=<databricks-PAT>;EnableArrow=0");
+      ds.setDriver("com.databricks.client.jdbc.Driver");
+      ds.setRequireLogin(false);
+
+      return ds;
+   }
+}
