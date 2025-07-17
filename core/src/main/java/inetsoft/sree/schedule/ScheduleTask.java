6062584953923cb3d779e7a4bee8176aec67e7d2
beauzhu <beauzhu@inetsoft.com>
2025-07-17 10:41:08 +0800

    Fix Bug #71945: Modify the email subject when a task fails in Community edition to exclude organization information.

--- core/src/main/java/inetsoft/sree/schedule/ScheduleTask.java
+++ core/src/main/java/inetsoft/sree/schedule/ScheduleTask.java
@@ -629,11 +629,21 @@ public class ScheduleTask implements Serializable, Cloneable, XMLSerializable {
                   // send task failed mail
                   if(SreeEnv.getBooleanProperty("schedule.options.taskFailed", "true", "CHECKED"))
                   {
+                     boolean isEnterprise = LicenseManager.getInstance().isEnterprise();
                      String subject = Catalog.getCatalog().getString(
-                        "em.scheduler.notification.taskFailedSub",
-                        SUtil.getTaskNameWithoutOrg(getTaskId()), SUtil.getTaskOrgName(getTaskId(), principal),
+                        "em.scheduler.notification.taskFailedSub.community", getName(),
                         (new SimpleDateFormat("hh:mma yyyy-MM-dd"))
-                        .format(new Date()));
+                           .format(new Date()));
+
+                     if(isEnterprise) {
+                        subject = Catalog.getCatalog().getString(
+                           "em.scheduler.notification.taskFailedSub",
+                           SUtil.getTaskNameWithoutOrg(getTaskId()),
+                           SUtil.getTaskOrgName(getTaskId(), principal),
+                           (new SimpleDateFormat("hh:mma yyyy-MM-dd"))
+                              .format(new Date()));
+                     }
+
                      String body = Catalog.getCatalog().getString(
                         "em.scheduler.notification.taskFailedBody",
                         exceptions.get(i));
