5da2eadf3711e9b1058f41c00dd4f84e8ffa17b7
fredaan <fredaan@inetsoft.com>
2025-07-31 18:27:17 +0800

    add ActionEvent and PropertyChangeEvent with orgID attribute.

---
+++ core/src/main/java/inetsoft/report/ActionEvent.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of StyleBI.
+ * Copyright (C) 2025  InetSoft Technology
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package inetsoft.report;
+
+import inetsoft.sree.security.OrganizationManager;
+
+public class ActionEvent extends java.awt.event.ActionEvent {
+   public ActionEvent(Object source, int actionID, String command) {
+      super(source, actionID, command);
+      this.orgID = OrganizationManager.getInstance().getCurrentOrgID();
+   }
+
+   public ActionEvent(Object source, int actionID, String command, String orgID) {
+      super(source, actionID, command);
+      this.orgID = orgID;
+   }
+
+   public String getOrgID() {
+      return orgID;
+   }
+
+   public void setOrgID(String orgID) {
+      this.orgID = orgID;
+   }
+
+   private String orgID;
+}
