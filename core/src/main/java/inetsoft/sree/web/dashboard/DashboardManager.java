19540f07947fbdd4fcee0c4c901497939597d1ed
nelson <nelsoncao@inetsoft.com>
2025-08-28 13:28:37 +0800

    fix Bug #72380, Bug #70778 has changed Anonymous user Identity type to User for the Dashboard, so should use user type for Anonymous user during get Dashboards.

--- core/src/main/java/inetsoft/sree/web/dashboard/DashboardManager.java
+++ core/src/main/java/inetsoft/sree/web/dashboard/DashboardManager.java
@@ -405,11 +405,11 @@ public class DashboardManager implements AutoCloseable {
    public synchronized String[] getUserDashboards(IdentityID userName) {
       init();
       SecurityProvider provider = SecurityEngine.getSecurity().getSecurityProvider();
-      User user;
+      Identity user;
 
       // treat user anonymous as a special role
       if(provider.isVirtual() || ClientInfo.ANONYMOUS.equals(userName.name)) {
-         return getDashboards(new DefaultIdentity(userName, Identity.ROLE));
+         user = new DefaultIdentity(userName, Identity.USER);
       }
       else if((user = provider.getUser(userName)) == null) {
          return new String[0];
