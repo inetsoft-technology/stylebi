51022d57b002d0bf9ebf786fdb8f560735a6da81
Catherine Li <catherineli@inetsoft.com>
2025-09-03 08:47:25 +0800

    Update ExportDialogModel.java
    add import for it

--- core/src/main/java/inetsoft/web/viewsheet/model/dialog/ExportDialogModel.java
+++ core/src/main/java/inetsoft/web/viewsheet/model/dialog/ExportDialogModel.java
@@ -21,6 +21,8 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
+import java.io.Serializable;
+
 /**
  * Data transfer object that represents the {@link ExportDialogModel}
  */
