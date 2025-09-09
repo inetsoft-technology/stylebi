6f9ac5fd3024e5ed7bec201c1ab62e87e928c1b1
hunterhu <hunterhu@inetsoft.com>
2025-09-09 13:30:59 +0800

    Fix merge conflicts. Related bugs: Bug #72456, Bug #72447.

---
+++ core/src/main/java/inetsoft/web/metrics/ActiveSessionScalingMetric.java
@@ -0,0 +1,38 @@
+/*
+ * This file is part of StyleBI.
+ * Copyright (C) 2025  InetSoft Technology
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package inetsoft.web.metrics;
+
+import inetsoft.sree.SreeEnv;
+import inetsoft.web.MapSessionRepository;
+
+public class ActiveSessionScalingMetric extends ScalingMetric {
+   public ActiveSessionScalingMetric(MapSessionRepository sessionRepository) {
+      super(false, 0);
+      this.sessionRepository = sessionRepository;
+   }
+
+   @Override
+   protected double calculate() {
+      double activeValue = Double.parseDouble(
+         SreeEnv.getProperty("metric.activeSession.metricValue", "0.5"));
+      return Math.clamp(sessionRepository.getActiveSessions().isEmpty() ? 0D : activeValue, 0D, 1D);
+   }
+
+   private final MapSessionRepository sessionRepository;
+}
