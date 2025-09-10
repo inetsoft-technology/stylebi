94dc520de16748a74f59a0f3cfe6527698caa5f1
Kenneth Sandoval <kenneth.sandoval@inetsoft.com>
2025-09-10 12:09:06 -0400

    Bug #72475  Store the expanded state of SelectionTrees in the assembly, so that it is kept when exporting

--- core/src/main/java/inetsoft/report/io/viewsheet/VSSelectionTreeHelper.java
+++ core/src/main/java/inetsoft/report/io/viewsheet/VSSelectionTreeHelper.java
@@ -51,8 +51,9 @@ public class VSSelectionTreeHelper extends VSSelectionListHelper {
       writeObjectBackground(info);
       StringBuilder sTitle = new StringBuilder();
       List<SelectionValue> dispList = new ArrayList<>();
-      boolean rootOnly = getExporter() != null && !getExporter().isExpandSelections();
-      prepareDisplayList(info, dispList, sTitle, false, rootOnly);
+      boolean expandAll = getExporter() == null || getExporter().isExpandSelections();
+      String[] expandedPaths = assembly.getExpandedValues();
+      prepareDisplayList(info, dispList, sTitle, false, expandAll, expandedPaths);
 
       // should be writeTitle after writeTree to control ShowType()
       if(dispList.size() > 0) {
@@ -72,7 +73,7 @@ public class VSSelectionTreeHelper extends VSSelectionListHelper {
     */
    protected void prepareDisplayList(SelectionTreeVSAssemblyInfo info,
                                      List<SelectionValue> dispList, StringBuilder sTitle,
-                                     boolean displayAll, boolean rootOnly)
+                                     boolean displayAll, boolean expandAll, String[] expandedPaths)
    {
       STR_MORE = catalog.getString("More") + "...";
       MORE_VALUE = new SelectionValue(STR_MORE, STR_MORE);
@@ -80,7 +81,7 @@ public class VSSelectionTreeHelper extends VSSelectionListHelper {
       MORE_VALUE.setLevel(0);
 
       CompositeSelectionValue csv = info.getCompositeSelectionValue();
-      info.visitCompositeChild(csv, dispList, true, rootOnly);  // populate dispList
+      info.visitCompositeChild(csv, dispList, true, expandAll, expandedPaths);  // populate dispList
 
       for(SelectionValue sv : dispList) {
          if(info.isValueVisible(sv)) {
