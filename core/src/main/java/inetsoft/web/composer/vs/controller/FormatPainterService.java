58ad52a6ed868c3ff285b28801378bd57e7aaddb
ngovus-inetsoft <nicholas.govus@inetsoft.com>
2025-10-15 10:11:15 -0400

    Bug #72799, need to reset underlying user format when resetting format (#2265)

--- core/src/main/java/inetsoft/web/composer/vs/controller/FormatPainterService.java
+++ core/src/main/java/inetsoft/web/composer/vs/controller/FormatPainterService.java
@@ -1421,6 +1421,11 @@ public class FormatPainterService {
                                     !reset && userFormat.isAlignmentValueDefined());
          }
 
+         if(reset) {
+            textFormat.getFormat().setFormat(null);
+            textFormat.getFormat().setFormatSpec(null);
+          }
+
          tofmt.getCSSFormat().setParentCSSParams(parentParams);
       }
    }
