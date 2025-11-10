df154a6b2a028e848467c7310030116083886d08
Jakub Kidacki <64920354+jkidacki-inetsoft@users.noreply.github.com>
2025-11-10 09:55:46 -0500

    Bug #73000 (#2355)
    Don't lose column aliases when removing the FormatTableLens

--- core/src/main/java/inetsoft/report/filter/ColumnMapFilter.java
+++ core/src/main/java/inetsoft/report/filter/ColumnMapFilter.java
@@ -731,6 +731,10 @@ public class ColumnMapFilter extends AbstractTableLens
       return type != null ? type : table != null ? table.getReportType() : null;
    }
 
+   public Object[] getHeaders() {
+      return headers;
+   }
+
    /**
     * ColumnMapFilter data descriptor.
     */
