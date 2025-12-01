bfaed0ce6a5bf78708146b8a14568b4547e4122c
Nicholas <nicholas.govus@inetsoft.com>
2025-12-01 14:04:00 -0500

    Bug #73131, pass orgid when processing user name change asset update

--- core/src/main/java/inetsoft/uql/asset/AssetEntry.java
+++ core/src/main/java/inetsoft/uql/asset/AssetEntry.java
@@ -2117,8 +2117,6 @@ public class AssetEntry implements AssetObject, Comparable<AssetEntry>, DataSeri
          newEntry.setProperty("__bookmark_id__", bookmarkId);
       }
 
-      newEntry.setProperty("onReport", getProperty("onReport"));
-
       return newEntry;
    }
 
