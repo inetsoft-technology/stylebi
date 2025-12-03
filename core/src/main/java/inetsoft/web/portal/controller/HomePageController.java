2c178c0d4b7ef8d5dffa5adf0f95f8da1d5586cd
Jason Shobe <jason.shobe@inetsoft.com>
2025-12-03 15:12:07 -0500

    Bug 73138, don't try to open viewsheet to generate Open Graph tags if the request does not come from a social app bot.

--- core/src/main/java/inetsoft/web/portal/controller/HomePageController.java
+++ core/src/main/java/inetsoft/web/portal/controller/HomePageController.java
@@ -17,10 +17,8 @@
  */
 package inetsoft.web.portal.controller;
 
-import inetsoft.sree.RepletRegistry;
 import inetsoft.sree.SreeEnv;
 import inetsoft.sree.portal.CustomThemesManager;
-import inetsoft.sree.portal.PortalThemesManager;
 import inetsoft.sree.security.*;
 import inetsoft.uql.asset.*;
 import inetsoft.uql.asset.internal.AssetUtil;
@@ -41,8 +39,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 @Controller
@@ -99,7 +102,7 @@ public class HomePageController {
       addAdditionalStyles(model, linkUri);
       addAdditionalScripts(model, linkUri);
       addDataSpaceTags(model, linkUri);
-      addOpenGraphTags(model, linkUri, request.getRequestURI());
+      addOpenGraphTags(model, linkUri, request.getRequestURI(), request.getHeader("User-Agent"));
 
       return model;
    }
@@ -134,56 +137,62 @@ public class HomePageController {
       return model;
    }
 
-   private void addOpenGraphTags(ModelAndView model, String linkUri, String requestUri) {
+   private void addOpenGraphTags(ModelAndView model, String linkUri, String requestUri,
+                                 String userAgent)
+   {
       String title = null;
       String description = null;
       String url = null;
 
-      try {
-         int index = requestUri.indexOf("/app/viewer/view/");
+      if(isBotAgent(userAgent)) {
+         // only open the viewsheet/report for bot agents that use the Open Graph tags as this can
+         // be a little time-consuming
+         try {
+            int index = requestUri.indexOf("/app/viewer/view/");
 
-         if(index >= 0) {
-            String path = requestUri.substring(index + 17);
-            url = linkUri + "app/viewer/view/" + path;
-            String assetId;
+            if(index >= 0) {
+               String path = requestUri.substring(index + 17);
+               url = linkUri + "app/viewer/view/" + path;
+               String assetId;
 
-            if(path.startsWith("global/")) {
-               String assetPath = path.substring(7);
-               assetId = "1^128^__NULL__^" + URLDecoder.decode(assetPath, "UTF-8");
-            }
-            else if(path.startsWith("user/")) {
-               path = path.substring(5);
-               index = path.indexOf('/');
-               String user = path.substring(0, index);
-               String assetPath = path.substring(index + 1);
-               assetId = "4^128^" + user + "^" + URLDecoder.decode(assetPath, "UTF-8");
-            }
-            else {
-               assetId = URLDecoder.decode(path, "UTF-8");
-            }
+               if(path.startsWith("global/")) {
+                  String assetPath = path.substring(7);
+                  assetId = "1^128^__NULL__^" + URLDecoder.decode(assetPath, StandardCharsets.UTF_8);
+               }
+               else if(path.startsWith("user/")) {
+                  path = path.substring(5);
+                  index = path.indexOf('/');
+                  String user = path.substring(0, index);
+                  String assetPath = path.substring(index + 1);
+                  assetId = "4^128^" + user + "^" + URLDecoder.decode(assetPath, StandardCharsets.UTF_8);
+               }
+               else {
+                  assetId = URLDecoder.decode(path, StandardCharsets.UTF_8);
+               }
 
-            AssetRepository repository = AssetUtil.getAssetRepository(false);
-            AssetEntry entry = AssetEntry.createAssetEntry(assetId);
+               AssetRepository repository = AssetUtil.getAssetRepository(false);
+               AssetEntry entry = AssetEntry.createAssetEntry(assetId);
 
-            if(entry != null) {
-               entry = repository.getAssetEntry(entry);
-            }
+               if(entry != null) {
+                  entry = repository.getAssetEntry(entry);
+               }
 
-            if(entry != null) {
-               title = entry.getAlias() == null ? entry.getPath() : entry.getAlias();
-               Viewsheet vs = (Viewsheet) repository.getSheet(entry, null, false, AssetContent.ALL);
-               description = vs.getViewsheetInfo().getDescription();
+               if(entry != null) {
+                  title = entry.getAlias() == null ? entry.getPath() : entry.getAlias();
+                  Viewsheet vs = (Viewsheet) repository.getSheet(entry, null, false, AssetContent.ALL);
+                  description = vs.getViewsheetInfo().getDescription();
 
-               index = title.lastIndexOf('/');
+                  index = title.lastIndexOf('/');
 
-               if(index >= 0) {
-                  title = title.substring(index + 1);
+                  if(index >= 0) {
+                     title = title.substring(index + 1);
+                  }
                }
             }
          }
-      }
-      catch(Exception e) {
-         LOG.error("Failed to get title and description for URL: {}", requestUri, e);
+         catch(Exception e) {
+            LOG.error("Failed to get title and description for URL: {}", requestUri, e);
+         }
       }
 
       if(title == null) {
@@ -198,19 +207,7 @@ public class HomePageController {
          url = linkUri + "/app/portal";
       }
 
-      String image = SreeEnv.getProperty("share.opengraph.image");
-
-      if(!StringUtils.hasText(image)) {
-         url = "/images/logo.png";
-      }
-
-      if(!image.startsWith("http") && !image.startsWith("//")) {
-         if(image.startsWith("/")) {
-            image = image.substring(1);
-         }
-
-         image = linkUri + image;
-      }
+      OpenGraphImage image = getOpenGraphImage(linkUri);
 
       // Normalize the strings, these are used as attribute values, so the whitespace that we allow
       // is a space (ASCII 32) character.
@@ -222,7 +219,9 @@ public class HomePageController {
       model.addObject("openGraphTitle", title);
       model.addObject("openGraphDescription", description);
       model.addObject("openGraphUrl", url);
-      model.addObject("openGraphImage", image);
+      model.addObject("openGraphImage", image.url);
+      model.addObject("openGraphImageWidth", image.width);
+      model.addObject("openGraphImageHeight", image.height);
    }
 
    private void addAdditionalStyles(ModelAndView model, String linkUri) {
@@ -241,7 +240,7 @@ public class HomePageController {
       }
    }
 
-   @SuppressWarnings("unchecked")
+   @SuppressWarnings({ "unchecked", "rawtypes" })
    private void addDataSpaceTags(ModelAndView model, String linkUrl, String dir) {
       for(String file : dataSpace.list(dir)) {
          String path = dir + "/" + file; // NOSONAR
@@ -294,7 +293,107 @@ public class HomePageController {
       return href;
    }
 
+   private boolean isBotAgent(String userAgent) {
+      if(userAgent == null) {
+         return false;
+      }
+
+      String lower = userAgent.toLowerCase();
+
+      for(String botAgent : botAgents) {
+         if(lower.contains(botAgent)) {
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+   private OpenGraphImage getOpenGraphImage(String linkUri) {
+      String image = SreeEnv.getProperty("share.opengraph.image");
+      URL resource = null;
+
+      if(!StringUtils.hasText(image)) {
+         image = "/images/logo.png";
+      }
+
+      if(image.startsWith("http")) {
+         try {
+            resource = URI.create(image).toURL();
+         }
+         catch(MalformedURLException e) {
+            LOG.warn("Invalid image URL: {}", image, e);
+         }
+      }
+      else if(image.startsWith("//")) {
+         if(linkUri.startsWith("http")) {
+            int index = linkUri.indexOf(':');
+
+            if(index > 0) {
+               try {
+                  resource = URI.create(linkUri.substring(0, index + 1) + image).toURL();
+               }
+               catch(MalformedURLException e) {
+                  throw new RuntimeException(e);
+               }
+            }
+         }
+      }
+      else {
+         if(image.startsWith("/")) {
+            image = image.substring(1);
+         }
+
+         resource = getClass().getResource("/inetsoft/web/resources/" + image);
+         image = linkUri + image;
+      }
+
+      imageLock.lock();
+
+      try {
+         if(cachedImage == null || !cachedImage.url.equals(image)) {
+            int width = -1;
+            int height = -1;
+
+            if(resource != null) {
+               try {
+                  BufferedImage img = ImageIO.read(resource);
+                  width = img.getWidth();
+                  height = img.getHeight();
+               }
+               catch(Exception e) {
+                  LOG.warn("Failed to resolve image URL: {}", resource, e);
+               }
+            }
+
+            cachedImage =  new OpenGraphImage(image, width, height);
+         }
+
+         return cachedImage;
+      }
+      finally {
+         imageLock.unlock();
+      }
+   }
+
+
    private final DataSpace dataSpace;
    private final boolean webAssetsExist;
+   private final Lock imageLock = new ReentrantLock();
+   private volatile OpenGraphImage cachedImage;
+   private static final Set<String> botAgents =
+      new HashSet<>(Arrays.asList("facebook", "slackbot", "linkedinbot", "twitterbot"));
    private static final Logger LOG = LoggerFactory.getLogger(HomePageController.class);
+
+   private static final class OpenGraphImage {
+      public OpenGraphImage(String url, int width, int height) {
+         this.url = url;
+         this.width = width;
+         this.height = height;
+      }
+
+      private final String url;
+      private final int width;
+      private final int height;
+   }
 }
