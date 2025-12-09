0b961f6ac50a57048ea3ad3d5ec6df5619e0101c
Kenneth Sandoval <kenneth.sandoval@inetsoft.com>
2025-12-09 11:35:51 -0500

    Bug #73160  Use default org as the resource org when editing through the all properties page

--- core/src/main/java/inetsoft/web/admin/properties/PropertiesController.java
+++ core/src/main/java/inetsoft/web/admin/properties/PropertiesController.java
@@ -47,7 +47,8 @@ public class PropertiesController {
 
    @Audited(
       actionName = ActionRecord.ACTION_NAME_DELETE,
-      objectType = ActionRecord.OBJECT_TYPE_EMPROPERTY
+      objectType = ActionRecord.OBJECT_TYPE_EMPROPERTY,
+      defaultOrg = true
    )
    @DeleteMapping("/api/admin/properties/delete")
    public void deleteProperty(Principal user,
@@ -66,7 +67,8 @@ public class PropertiesController {
 
    @Audited(
       actionName = ActionRecord.ACTION_NAME_EDIT,
-      objectType = ActionRecord.OBJECT_TYPE_EMPROPERTY
+      objectType = ActionRecord.OBJECT_TYPE_EMPROPERTY,
+      defaultOrg = true
    )
    @PutMapping("/api/admin/properties/edit")
    public void editProperty(Principal user,
