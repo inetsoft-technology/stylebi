c25e6b3e1e1c0f574c4764fca90db22cdfe4d2c3
Larry Liang <larry.liang@inetsoft.com>
2026-02-10 13:42:07 -0800

    Bug #73834, handle exception during shutdown

--- core/src/main/java/inetsoft/sree/internal/ScheduleThread.java
+++ core/src/main/java/inetsoft/sree/internal/ScheduleThread.java
@@ -20,6 +20,7 @@ package inetsoft.sree.internal;
 import inetsoft.sree.SreeEnv;
 import inetsoft.sree.internal.cluster.Cluster;
 import inetsoft.sree.schedule.ScheduleClient;
+import inetsoft.util.SingletonManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -37,14 +38,20 @@ public class ScheduleThread implements Runnable {
       boolean sendEmail = true;
 
       while(!stopped) {
-         if(!client.isReady() && !stopped) {
-            if(sendEmail) {
-               sendScheduleOffEmail();
-               sendEmail = false;
+         try {
+            if(!client.isReady() && !stopped) {
+               if(sendEmail) {
+                  sendScheduleOffEmail();
+                  sendEmail = false;
+               }
+            }
+            else {
+               sendEmail = true;
             }
          }
-         else {
-            sendEmail = true;
+         catch(SingletonManager.ResurrectException e) {
+            LOG.debug("Server is shutting down, stopping schedule monitor thread");
+            break;
          }
 
          try {
