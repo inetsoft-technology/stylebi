4b2cc16f57ad680642ec8ad52760273e2b41865f
Catherine Li <catherineli@inetsoft.com>
2026-03-04 09:18:39 +0800

    Update CubeVSAQuery.java
    coding review

--- core/src/main/java/inetsoft/report/composition/execution/CubeVSAQuery.java
+++ core/src/main/java/inetsoft/report/composition/execution/CubeVSAQuery.java
@@ -118,7 +118,7 @@ public abstract class CubeVSAQuery extends DataVSAQuery {
       }
 
       appendAggCalcField(table, tname);
-      appendCalcField(table, tname, true, true, getViewsheet());
+      appendCalcFieldWithType(table, tname, true, true, getViewsheet());
 
       if(!analysis) {
          ChartVSAssembly chart = box.getBrushingChart(vname);
