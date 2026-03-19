c264dfc16d295abcfdff2c3d1866e4f4ace78dad
beauzhu <beauzhu@inetsoft.com>
2026-03-19 14:05:22 +0800

    Clicking the Wiz Portal icon in the navigation bar will route to /portal/tab/wiz-portal, and the content will be displayed in full screen within the tab-body area, consistent with the behavior of the Data and Schedule tabs.

---
+++ web/projects/shared/wiz-portal/wiz-portal-tab.component.ts
@@ -0,0 +1,46 @@
+/*
+ * This file is part of StyleBI.
+ * Copyright (C) 2026  InetSoft Technology
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Component } from "@angular/core";
+import { WizPortalService } from "./wiz-portal.service";
+
+@Component({
+   selector: "p-wiz-portal-tab",
+   templateUrl: "./wiz-portal-tab.component.html",
+   styleUrls: ["./wiz-portal-tab.component.scss"]
+})
+export class WizPortalTabComponent {
+   get styleBIUrl(): string {
+      return this.wizPortalService.styleBIUrl;
+   }
+
+   get wizServiceUrl(): string {
+      return this.wizPortalService.wizServiceUrl;
+   }
+
+   get userId(): string {
+      return this.wizPortalService.userId;
+   }
+
+   get domain(): string {
+      return this.wizPortalService.domain;
+   }
+
+   constructor(private wizPortalService: WizPortalService) {
+   }
+}
