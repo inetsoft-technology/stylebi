5b03b8145c6e03cdbcf00d10ecad6b7667c570d0
nelson <nelsoncao@inetsoft.com>
2026-04-10 15:11:52 +0800

    implements get tables of db.

---
+++ core/src/main/java/inetsoft/web/wiz/model/DatabaseTableInfo.java
@@ -0,0 +1,73 @@
+/*
+ * This file is part of StyleBI.
+ * Copyright (C) 2026  InetSoft Technology
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package inetsoft.web.wiz.model;
+
+import inetsoft.uql.asset.AssetEntry;
+
+public class DatabaseTableInfo {
+   public String getType() {
+      return "table";
+   }
+
+   public String getDatabase() {
+      return database;
+   }
+
+   public void setDatabase(String database) {
+      this.database = database;
+   }
+
+   public String getCatalog() {
+      return catalog;
+   }
+
+   public void setCatalog(String catalog) {
+      this.catalog = catalog;
+   }
+
+   public String getSchema() {
+      return schema;
+   }
+
+   public void setSchema(String schema) {
+      this.schema = schema;
+   }
+
+   public String getTable() {
+      return table;
+   }
+
+   public void setTable(String table) {
+      this.table = table;
+   }
+
+   public AssetEntry getAssetData() {
+      return assetData;
+   }
+
+   public void setAssetData(AssetEntry assetData) {
+      this.assetData = assetData;
+   }
+
+   private String database;
+   private String catalog;
+   private String schema;
+   private String table;
+   private AssetEntry assetData;
+}
