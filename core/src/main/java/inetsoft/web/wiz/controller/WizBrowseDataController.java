4d47477dcd6d1e3c2c97cf7b7c4192d8b0d6b8d1
catherineli <catherineli@inetsoft.com>
2026-04-15 10:25:35 +0800

    get condition values by browse data controller.

---
+++ core/src/main/java/inetsoft/web/wiz/controller/WizBrowseDataController.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of StyleBI.
+ * Copyright (C) 2026  InetSoft Technology
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package inetsoft.web.wiz.controller;
+
+import inetsoft.web.binding.drm.DataRefModel;
+import inetsoft.web.composer.model.BrowseDataModel;
+import inetsoft.web.wiz.service.WizBrowseDataService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/wiz")
+public class WizBrowseDataController {
+
+   public WizBrowseDataController(WizBrowseDataService wizBrowseDataService) {
+      this.wizBrowseDataService = wizBrowseDataService;
+   }
+
+   /**
+    * Browse distinct values for a worksheet column, using a VS runtimeId to locate
+    * the underlying worksheet.
+    *
+    * @param runtimeId    VS runtime ID (vsId)
+    * @param assemblyName worksheet table/assembly name that owns the column
+    * @param dataRefModel column descriptor (classType, name, attribute, …)
+    * @param principal    current user
+    */
+   @PostMapping(value = "/vs/condition/browse-data", produces = MediaType.APPLICATION_JSON_VALUE)
+   public BrowseDataModel browseData(
+      @RequestParam("runtimeId") String runtimeId,
+      @RequestParam("assemblyName") String assemblyName,
+      @RequestBody DataRefModel dataRefModel,
+      Principal principal) throws Exception
+   {
+      return wizBrowseDataService.browseData(runtimeId, assemblyName, dataRefModel, principal);
+   }
+
+   private final WizBrowseDataService wizBrowseDataService;
+}
