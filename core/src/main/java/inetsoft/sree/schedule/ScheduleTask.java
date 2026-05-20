6b3f77cfa6588b5163f821a76f5530bda9a6f554
milotalon <176326990+milotalon@users.noreply.github.com>
2026-05-20 14:51:01 -0400

    remove duplicate property retrieval

--- core/src/main/java/inetsoft/sree/schedule/ScheduleTask.java
+++ core/src/main/java/inetsoft/sree/schedule/ScheduleTask.java
@@ -529,20 +529,19 @@ public class ScheduleTask implements Serializable, Cloneable, XMLSerializable {
       List<Future> futures = new ArrayList<>();
       boolean waited = false;
       long startTime = System.currentTimeMillis();
+      long timeout;
+
+      try {
+         timeout = Long.parseLong(SreeEnv.getProperty("schedule.task.timeout"));
+      }
+      catch(NumberFormatException e) {
+         timeout = 600000L;
+      }
 
       for(int i = 0; i < acts.size(); i++) {
          final ScheduleAction act = acts.elementAt(i);
 
          if(!waited && act instanceof MVAction && ((MVAction) act).isSequenced()) {
-            long timeout;
-
-            try {
-               timeout = Long.parseLong(SreeEnv.getProperty("schedule.task.timeout"));
-            }
-            catch(NumberFormatException e) {
-               timeout = 600000L;
-            }
-
             for(Future future : futures) {
                try {
                   if(timeout > 0) {
@@ -626,14 +625,6 @@ public class ScheduleTask implements Serializable, Cloneable, XMLSerializable {
       int threshold = (cycleInfo != null && cycleInfo.isExceedNotify())
          ? cycleInfo.getThreshold() : 0;
       int time = 0;
-      long timeout;
-
-      try {
-         timeout = Long.parseLong(SreeEnv.getProperty("schedule.task.timeout"));
-      }
-      catch(NumberFormatException e) {
-         timeout = 600000L;
-      }
 
       // only after all the replet actions are over, should the task be over
       synchronized(this) {
