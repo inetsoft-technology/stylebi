169790711fb0d1b20e9700d7536fb3c7a2dbdf25
Nicholas <nicholas.govus@inetsoft.com>
2026-05-29 14:14:07 -0400

    Bug #75214, Add fallback aggregate expression lookup and reimport missing services to restore functionality after merge, code style

--- core/src/main/java/inetsoft/report/composition/execution/TimeSliderVSAQuery.java
+++ core/src/main/java/inetsoft/report/composition/execution/TimeSliderVSAQuery.java
@@ -356,7 +356,7 @@ public class TimeSliderVSAQuery extends AbstractSelectionVSAQuery {
          // Extract the column name from inside the parentheses and retry.
          if(ref == null) {
             int idx1 = alias.indexOf('(');
-            int idx2 = alias.lastIndexOf(')');
+            int idx2 = alias.indexOf(')');
 
             if(idx1 > 0 && idx2 > idx1) {
                String baseAttr = alias.substring(idx1 + 1, idx2);
