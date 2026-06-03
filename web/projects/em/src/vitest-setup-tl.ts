030859651a6d238928fe564ff55152b57a9ed715
bonnieshi <bonnieshi@inetsoft.com>
2026-06-03 15:38:55 +0800

    Add test:em:tl target and normalize test scripts

---
+++ web/projects/em/src/vitest-setup-tl.ts
@@ -0,0 +1,26 @@
+/*
+ * This file is part of StyleBI.
+ * Copyright (C) 2024  InetSoft Technology
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// TL (testing-library) test setup for the em project. Starts the MSW server
+// lifecycle so all *.tl.spec.ts files in em can intercept HTTP requests.
+import { afterAll, afterEach, beforeAll } from "vitest";
+import { server } from "../../../mocks/server";
+
+beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
