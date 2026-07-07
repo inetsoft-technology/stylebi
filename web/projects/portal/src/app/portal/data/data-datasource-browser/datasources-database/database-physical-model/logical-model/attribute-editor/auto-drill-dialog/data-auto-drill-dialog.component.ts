f9b05b1849005b9798d4350b726cbe1571554ee4
Sara Peng <76981987+sarapeng86@users.noreply.github.com>
2026-07-07 21:31:08 +0800

    Fix unit test bugs (#4185)
    * fix bug-75584
    
    * fix bug-75586
    
    * fix bug-75587
    
    * fix bug-75589
    
    * fix bug-75590

--- web/projects/portal/src/app/portal/data/data-datasource-browser/datasources-database/database-physical-model/logical-model/attribute-editor/auto-drill-dialog/data-auto-drill-dialog.component.ts
+++ web/projects/portal/src/app/portal/data/data-datasource-browser/datasources-database/database-physical-model/logical-model/attribute-editor/auto-drill-dialog/data-auto-drill-dialog.component.ts
@@ -360,7 +360,7 @@ export class AutoDrillDialog implements OnInit, AfterViewInit {
          }
       }
 
-      return newNamePre + existIndexs.length;
+      return existIndexs.length == 0 ? newNamePre : newNamePre + existIndexs.length;
    }
 
    /**
